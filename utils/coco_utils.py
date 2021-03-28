da32061a6e1db55bc1c0ed538f3fe1e07233fe91
Jan <jan@onspecta.com>
2021-03-28 01:48:51 +0100

    Make some functions inside the class private

--- utils/coco_utils.py
+++ utils/coco_utils.py
@@ -29,31 +29,31 @@ class COCODataset:
         except KeyError:
             print_goodbye_message_and_die("COCO dataset directory has not been specified with COCO_DIR flag")
 
-    def generate_coco_filename(self):
+    def __generate_coco_filename(self):
         image_id_as_str = str(self.image_id)
         return self.images_filename_base[:-len(image_id_as_str)] + image_id_as_str + self.images_filename_extension
 
-    def find_next_images_path(self):
+    def __find_next_images_path(self):
         while self.image_id < self.max_image_id:
-            potential_path = pathlib.PurePath(self.coco_directory, self.generate_coco_filename())
+            potential_path = pathlib.PurePath(self.coco_directory, self.__generate_coco_filename())
             if pathlib.Path(potential_path).is_file():
                 return potential_path
             self.image_id += 1
         return None
 
-    def get_path_to_image(self):
-        path = self.find_next_images_path()
+    def __get_path_to_image(self):
+        path = self.__find_next_images_path()
         if not path:
             if self.run_in_loop:
                 self.image_id = 0
-                path = self.find_next_images_path()
+                path = self.__find_next_images_path()
             if not path:
                 print_goodbye_message_and_die(
                     f"Either end of dataset has been reached or "
                     f"files were not found under the directory {self.coco_directory}")
         return str(path)
 
-    def resize_and_crop_image(self, image_array):
+    def __resize_and_crop_image(self, image_array):
         image_height = image_array.shape[0]
         image_width = image_array.shape[1]
         target_height = self.shape[0]
@@ -88,19 +88,19 @@ class COCODataset:
         padded_array[lower_boundary_h:upper_boundary_h, lower_boundary_w:upper_boundary_w] = image_array
         return padded_array
 
-    def get_next_image(self):
-        image_array = cv2.imread(self.get_path_to_image())
+    def __get_next_image(self):
+        image_array = cv2.imread(self.__get_path_to_image())
         if self.allow_distortion:
             image_array = cv2.resize(image_array, self.shape)
         else:
-            image_array = self.resize_and_crop_image(image_array)
+            image_array = self.__resize_and_crop_image(image_array)
         self.image_id += 1
         return np.expand_dims(image_array, axis=0).astype(self.input_data_type)
 
     def get_input_array(self):
-        input_array = self.get_next_image()
+        input_array = self.__get_next_image()
         for _ in range(1, self.batch_size):
-            input_array = np.concatenate((input_array, self.get_next_image()), axis=0)
+            input_array = np.concatenate((input_array, self.__get_next_image()), axis=0)
         if self.pre_processing_func:
             input_array = self.pre_processing_func(input_array)
         return input_array
@@ -116,4 +116,3 @@ for _ in range(1000):
     _ = coco_dataset.get_input_array()
     end = time.time()
     print(end-start)
-print(tensor.shape)
