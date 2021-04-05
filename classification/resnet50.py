7523191a58249a1d64f9ffffad57cae05f269325
MarcelWilnicki <marcel@onspecta.com>
2021-04-05 23:39:33 +0200

    WIP

--- classification/resnet50.py
+++ classification/resnet50.py
@@ -10,7 +10,7 @@ from utils.vggpreprocessor import vgg_preprocessor
 from utils.mix import calculate_images
 
 
-labels = 'model_zoo/utils/ILSVRC2012_validation_ground_truth.txt'
+labels = '/model_zoo/utils/ILSVRC2012_validation_ground_truth.txt'
 
 
 def parse_args():
@@ -26,6 +26,12 @@ def parse_args():
 
 def benchmark(model_path, batch_size, timeout_in_minutes=1):
 
+    # Read labels
+    file_with_labels = open(labels, 'r')
+    lines = file_with_labels.readlines()
+    labels_iterator = iter(lines)
+
+    # loading graph and starting tensorflow session
     graph = load_graph(model_path)
     input_tensor = graph.get_tensor_by_name("input_tensor:0")
     output_tensor = graph.get_tensor_by_name("softmax_tensor:0")
@@ -33,28 +39,63 @@ def benchmark(model_path, batch_size, timeout_in_minutes=1):
 
     number_of_images = calculate_images()
     num_of_iter = number_of_images/batch_size
+    image_net = ImageNet(batch_size)
 
-    image_net = ImageNet()
-    preprocessed_input = image_net.get_input_tensor(batch_size, (224, 224), vgg_preprocessor)
-
-    timeout = time.time() + 60 * int(timeout_in_minutes)
-
+    # benchmarking
     total_time = 0.0
-    start = time.time()
+    correct_count = 0
+    incorrect_count = 0
+    image_count = 0
 
+    # timeout
+    timeout = time.time() + 60 * int(timeout_in_minutes)
     for n in range(0, int(num_of_iter)):
         if time.time() > timeout:
             break
 
+        image_count += 1
+        path, preprocessed_input = image_net.get_input_tensor((224, 224), vgg_preprocessor)
+
+        print(path)
+
+        start = time.time()
         result = sess.run(output_tensor, feed_dict={input_tensor: preprocessed_input})
-        print(n)
+        end = time.time()
+        total_time += (end - start)
 
         max_value_index = np.where(result == np.max(result))[1]
         max_value_index = int(max_value_index)
         print(max_value_index)
 
+        if max_value_index == next(labels_iterator):
+            correct_count += 1
+        else:
+            incorrect_count += 1
+
     end = time.time()
     total_time += (end - start)
+
+    print('------------------------------')
+
+    accuracy = ((correct_count / image_count) * 100)
+    print("top-1 accuracy: %.2f" % accuracy, "%")
+
+    print('------------------------------')
+
+    minutes = total_time / 60
+    print("total time of run inferences: %.2f" % minutes, "Minutes")
+
+    print('------------------------------')
+
+    latency_in_miliseconds = (total_time / image_count) * 1000
+    print("average latency in miliseconds: %.4f" % latency_in_miliseconds)
+
+    print('------------------------------')
+
+    latency_in_fps = image_count / total_time
+    print("average latency in fps: %.4f" % latency_in_fps)
+
+    print('------------------------------')
 
 
 def main():
--- utils/imagenet.py
+++ utils/imagenet.py
@@ -6,18 +6,18 @@ import numpy as np
 import cv2
 from utils.mix import batch
 
-labels = 'model_zoo/utils/ILSVRC2012_validation_ground_truth.txt'
-
 
 class ImageNet:
 
     # variable which you can set by initializing ImageNet
 
-    def __init__(self):
+    def __init__(self, batch_size):
 
         self.images_path = os.environ['IMAGES_PATH']
         self.number_of_images = 50000
         self.image_count = 0
+        self.parent_list = os.listdir(self.images_path)
+        self.g = batch(self.parent_list, batch_size)
 
         if not path.exists(self.images_path):
             print("path doesn't exist")
@@ -25,18 +25,16 @@ class ImageNet:
         else:
             print('works')
 
-    def get_input_tensor(self, batch_size, input_shape, preprocess):
+    def get_input_tensor(self, input_shape, preprocess):
         final_batch = np.empty((0, 224, 224, 3))
 
-        parent_list = os.listdir(self.images_path)
-        g = batch(parent_list, batch_size)
+        for i in self.g.__next__():
 
-        for i in g.__next__():
-
+            img_path = os.path.join(self.images_path, i)
             img = cv2.imread(os.path.join(self.images_path, i))
             resized_img = cv2.resize(img, input_shape)
             preprocessed_img = preprocess(resized_img)
             final_batch = np.append(final_batch, preprocessed_img, axis=0)
 
-        return final_batch
+        return img_path, final_batch
 
