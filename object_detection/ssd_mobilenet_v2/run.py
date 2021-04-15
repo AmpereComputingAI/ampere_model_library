0c0f8b3ed8a12ab0771e329add6195777617acf8
Jan <jan@onspecta.com>
2021-04-16 01:28:48 +0200

    clean-up

--- object_detection/ssd_mobilenet_v2/run.py
+++ object_detection/ssd_mobilenet_v2/run.py
@@ -1,27 +1,62 @@
-import utils.new_coco as coco_utils
-import utils.tf as tf_utils
-import tensorflow.compat.v1 as tf
-import time
 import os
+import time
+import argparse
+import utils.tf as tf_utils
+import utils.coco as coco_utils
+import tensorflow.compat.v1 as tf
 
-TIMEOUT = 10000.0
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run SSD MobileNet v2 model.")
+    parser.add_argument("-m", "--model_path",
+                        type=str, required=True,
+                        help="path to the model")
+    parser.add_argument("-p", "--precision",
+                        type=str, choices=["fp32", "fp16", "int8"], required=True,
+                        help="precision of the model provided")
+    parser.add_argument("-b", "--batch_size",
+                        type=int, default=1,
+                        help="batch size to feed the model with")
+    parser.add_argument("--timeout",
+                        type=float, default=60.0,
+                        help="timeout in seconds")
+    parser.add_argument("--num_runs",
+                        type=int,
+                        help="number of passes through network to execute")
+    parser.add_argument("--images_path",
+                        type=str,
+                        help="path to directory with COCO validation images")
+    parser.add_argument("--anno_path",
+                        type=str,
+                        help="path to file with validation annotations")
+    return parser.parse_args()
 
 
-def run_ssd_mn_v2_with_tf(number_of_runs=100, batch_size=1, shape=(640, 640)):
-    coco = coco_utils.COCODataset(batch_size, "COCO_val2014_000000000000")
+def run_tf_fp32(model_path, batch_size, num_of_runs, timeout, images_path, anno_path):
+    shape = (640, 640)
+    coco = coco_utils.COCODataset(batch_size, "COCO_val2014_000000000000", images_path, anno_path)
 
     runner = tf_utils.TFFrozenModelRunner(
-        "ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb",
-        ["detection_classes:0", "detection_boxes:0", "detection_scores:0", "num_detections:0"])
+        model_path,
+        ["detection_classes:0", "detection_boxes:0", "detection_scores:0", "num_detections:0"]
+    )
 
+    iter = 0
     start = time.time()
-    while time.time() - start < TIMEOUT:
+    while True:
+        if num_of_runs is None:
+            if time.time() - start > timeout:
+                break
+        elif not iter < num_of_runs:
+            break
+
         try:
             runner.set_input_tensor("image_tensor:0", coco.get_input_array(shape))
         except coco.OutOfCOCOImages:
             break
 
         output = runner.run()
+        iter += 1
 
         for i in range(batch_size):
             for d in range(int(output["num_detections:0"][i])):
@@ -36,5 +71,15 @@ def run_ssd_mn_v2_with_tf(number_of_runs=100, batch_size=1, shape=(640, 640)):
     runner.print_performance_metrics(batch_size)
 
 
+def main():
+    args = parse_args()
+    if args.precision == "fp32":
+        run_tf_fp32(
+            args.model_path, args.batch_size, args.num_runs, args.timeout, args.images_path, args.anno_path
+        )
+    else:
+        assert False, "Other precisions not supported yet."
+
+
 if __name__ == "__main__":
-    run_ssd_mn_v2_with_tf()
+    main()
