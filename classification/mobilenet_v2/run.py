b3ac1d71eedd80bc51ab6c37ae0ffae6cbe381ae
MarcelWilnicki <marcel@onspecta.com>
2021-04-16 21:06:42 +0200

    update mobilenet run file

--- classification/mobilenet_v2/run.py
+++ classification/mobilenet_v2/run.py
@@ -9,19 +9,32 @@ from utils.tf_utils import TFFrozenModelRunner
 def parse_args():
     parser = argparse.ArgumentParser(description='benchmark resnet model')
 
-    parser.add_argument('-m', '--model_path', type=str, required=True, help='Path to frozen model.')
-    parser.add_argument('-t', '--timeout', type=int, default=60, help='Timeout for processing evaluation'
-                                                                      'dataset in seconds. Default is 60 seconds.')
-    parser.add_argument('-b', '--batch_size', type=int, default=1, help='Batch size for processing evaluation'
-                                                                        'dataset. Possible values: 1, 4, 16, 32, 64.'
-                                                                        'Default value is 1.')
-    parser.add_argument('-p', '--precision', type=str, required=True, choices=['fp32', 'fp16', 'int8'],
+    parser.add_argument('-m', '--model_path',
+                        type=str, required=True,
+                        help='Path to frozen model.')
+    parser.add_argument('-p', '--precision',
+                        type=str, choices=['fp32', 'fp16', 'int8'],
+                        required=True,
                         help='Specify the precision of'
-                             'the model provided with'
-                             ' --model_path argument.')
-    parser.add_argument('-i', '--images_path', type=str, help="Path to ImageNet validation dataset.")
-    parser.add_argument('-l', '--labels_path', type=str, help="Path to validation labels.")
+                              'the model provided.')
+    parser.add_argument('-b', '--batch_size',
+                        type=int, default=1,
+                        help='Batch size for processing evaluation'
+                              'dataset. Possible values: 1, 4, 16, 32, 64. '
+                              'Default value is 1.')
+    parser.add_argument('--timeout',
+                        type=int, default=60,
+                        help='Timeout for processing evaluation'
+                             'dataset in seconds. Default is 60 seconds.')
+    parser.add_argument('--images_path',
+                        type=str,
+                        help="Path to ImageNet validation dataset.")
+    parser.add_argument('--labels_path',
+                        type=str,
+                        help="Path to validation labels.")
+
     return parser.parse_args()
+
 
 
 def benchmark(model_path, batch_size, images_path, labels_path, timeout):
@@ -38,7 +51,10 @@ def benchmark(model_path, batch_size, images_path, labels_path, timeout):
     while time.time() - time_of_start < timeout:
 
         # preprocess input
-        preprocessed_input = image_net.get_input_tensor((224, 224), inception_preprocessor)
+        try:
+            preprocessed_input = image_net.get_input_tensor((224, 224), inception_preprocessor)
+        except image_net.OutOfImageNetImages:
+            break
 
         # set input tensor and run interference
         tf_runner.set_input_tensor('input:0', preprocessed_input)
