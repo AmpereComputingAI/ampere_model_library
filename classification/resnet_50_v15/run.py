aa8a0eb3853fcb65a05c02dafe88a93646d44d75
marcel <marcel.wilnicki@gmail.com>
2021-04-21 22:06:13 +0000

    add int8 model benchmarking to run scripts

--- classification/resnet_50_v15/run.py
+++ classification/resnet_50_v15/run.py
@@ -1,6 +1,7 @@
 import argparse
 from utils.imagenet import ImageNet
 from utils.tf import TFFrozenModelRunner
+from utils.tflite import TFLiteRunner
 from utils.benchmark import run_model
 
 
@@ -51,8 +52,8 @@ def run_tf_fp32(model_path, batch_size, num_of_runs, timeout, images_path, label
 
 
 def run_tf_fp16(model_path, batch_size, num_of_runs, timeout, images_path, labels_path):
+
     def run_single_pass(tf_runner, imagenet):
-        print('i am here')
         shape = (224, 224)
         tf_runner.set_input_tensor("input_tensor:0", imagenet.get_input_array(shape))
         output = tf_runner.run()
@@ -71,6 +72,26 @@ def run_tf_fp16(model_path, batch_size, num_of_runs, timeout, images_path, label
     return run_model(run_single_pass, runner, dataset, batch_size, num_of_runs, timeout)
 
 
+def run_tflite_int8(model_path, batch_size, num_of_runs, timeout, images_path, labels_path):
+
+    def run_single_pass(tflite_runner, imagenet):
+        shape = (224, 224)
+        tflite_runner.set_input_tensor(tflite_runner.input_details[0]['index'], imagenet.get_input_array(shape))
+        tflite_runner.run()
+        test = tflite_runner.get_output_tensor(tflite_runner.output_details[0]['index'])
+        imagenet.submit_predictions(
+            0,
+            imagenet.extract_top1(test),
+            imagenet.extract_top5(test)
+        )
+
+    dataset = ImageNet(batch_size, "RGB", images_path, labels_path,
+                       pre_processing_approach="Inception", is1001classes=True)
+    runner = TFLiteRunner(model_path)
+
+    return run_model(run_single_pass, runner, dataset, batch_size, num_of_runs, timeout)
+
+
 def main():
     args = parse_args()
     if args.precision == "fp32":
@@ -79,6 +100,10 @@ def main():
         )
     elif args.precision == "fp16":
         run_tf_fp16(
+            args.model_path, args.batch_size, args.num_runs, args.timeout, args.images_path, args.labels_path
+        )
+    elif args.precision == "int8":
+        run_tflite_int8(
             args.model_path, args.batch_size, args.num_runs, args.timeout, args.images_path, args.labels_path
         )
     else:
