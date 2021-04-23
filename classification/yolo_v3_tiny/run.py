4dc67e5c2b1a69dcc8de72b2bef7f301d0558d08
marcel <marcel.wilnicki@gmail.com>
2021-04-23 19:51:45 +0000

    wip

---
+++ classification/yolo_v3_tiny/run.py
@@ -0,0 +1,74 @@
+import argparse
+import sys
+
+from utils.imagenet import ImageNet
+from utils.tf import TFFrozenModelRunner
+from utils.coco import COCODataset
+from utils.tflite import TFLiteRunner
+from utils.benchmark import run_model
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run MobileNet v2 model.")
+    parser.add_argument("-m", "--model_path",
+                        type=str, required=True,
+                        help="path to the model")
+    parser.add_argument("-p", "--precision",
+                        type=str, choices=["fp32", "fp16", "int8"], required=True,
+                        help="precision of the model provided")
+    parser.add_argument("-b", "--batch_size",
+                        type=int, default=1,
+                        help="batch size to feed the model with")
+    parser.add_argument("--timeout",
+                        type=float, default=60.0,
+                        help="timeout in seconds")
+    parser.add_argument("--num_runs",
+                        type=int,
+                        help="number of passes through network to execute")
+    parser.add_argument("--images_path",
+                        type=str,
+                        help="path to directory with ImageNet validation images")
+    parser.add_argument("--labels_path",
+                        type=str,
+                        help="path to file with validation labels")
+    return parser.parse_args()
+
+
+def run_tf_fp32(model_path, batch_size, num_of_runs, timeout, images_path, labels_path):
+
+    def run_single_pass(tf_runner, imagenet):
+        shape = (416, 416)
+        tf_runner.set_input_tensor("inputs:0", imagenet.get_input_array(shape))
+        output = tf_runner.run()
+        print(type(output))
+        # print(output)
+        test = output["output_boxes:0"]
+        print(test.shape)
+        print(test)
+        count = 0
+        # for i in range(85):
+        #     print(test[0][0][i])
+        #     count += 1
+
+    dataset = COCODataset(batch_size, "RGB", "COCO_val2014_000000000000", images_path, labels_path)
+
+    runner = TFFrozenModelRunner(model_path, ["output_boxes:0"])
+
+    return run_model(run_single_pass, runner, dataset, batch_size, num_of_runs, timeout)
+
+
+def main():
+    args = parse_args()
+    if args.precision == "fp32":
+        run_tf_fp32(
+            args.model_path, args.batch_size, args.num_runs, args.timeout, args.images_path, args.labels_path
+        )
+    else:
+        assert False
+
+
+if __name__ == "__main__":
+    main()
+
+
+
