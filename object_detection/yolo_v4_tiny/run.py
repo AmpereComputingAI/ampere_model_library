094dc65bbff59f3fda5a1df5e9b5843dfe42655c
Jan <jan@onspecta.com>
2021-04-27 13:39:28 +0200

    yolo v4

---
+++ object_detection/yolo_v4_tiny/run.py
@@ -0,0 +1,118 @@
+import os
+import time
+import argparse
+import utils.misc as utils
+from utils.coco import COCODataset
+from utils.tflite import TFLiteRunner
+from utils.tf import TFSavedModelRunner
+from utils.benchmark import run_model
+import tensorflow as tf
+
+
+COCO_MAP = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 11, 12: 13, 13: 14, 14: 15, 15: 16,
+            16: 17, 17: 18, 18: 19, 19: 20, 20: 21, 21: 22, 22: 23, 23: 24, 24: 25, 25: 27, 26: 28, 27: 31, 28: 32,
+            29: 33, 30: 34, 31: 35, 32: 36, 33: 37, 34: 38, 35: 39, 36: 40, 37: 41, 38: 42, 39: 43, 40: 44, 41: 46,
+            42: 47, 43: 48, 44: 49, 45: 50, 46: 51, 47: 52, 48: 53, 49: 54, 50: 55, 51: 56, 52: 57, 53: 58, 54: 59,
+            55: 60, 56: 61, 57: 62, 58: 63, 59: 64, 60: 65, 61: 67, 62: 70, 63: 72, 64: 73, 65: 74, 66: 75, 67: 76,
+            68: 77, 69: 78, 70: 79, 71: 80, 72: 81, 73: 82, 74: 84, 75: 85, 76: 86, 77: 87, 78: 88, 79: 89, 80: 90}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run SSD MobileNet v2 model.")
+    parser.add_argument("-m", "--model_path",
+                        type=str, required=True,
+                        help="path to the model")
+    parser.add_argument("-p", "--precision",
+                        type=str, choices=["fp32", "int8"], required=True,
+                        help="precision of the model provided")
+    parser.add_argument("-b", "--batch_size",
+                        type=int, default=1,
+                        help="batch size to feed the model with")
+    parser.add_argument("--timeout",
+                        type=float, default=60.0,
+                        help="timeout in seconds")
+    parser.add_argument("--num_runs",
+                        type=int,
+                        help="number of passes through network to execute")
+    parser.add_argument("--images_path",
+                        type=str,
+                        help="path to directory with COCO validation images")
+    parser.add_argument("--anno_path",
+                        type=str,
+                        help="path to file with validation annotations")
+    return parser.parse_args()
+
+
+def run_tf_fp32(model_path, batch_size, num_of_runs, timeout, images_path, anno_path):
+    def run_single_pass(tf_runner, coco):
+        shape = (416, 416)
+        output = tf_runner.run(coco.get_input_array(shape))
+        bboxes = output["tf.concat_12"][:, :, 0:4]
+        preds = output["tf.concat_12"][:, :, 4:]
+        detection_boxes, detection_scores, detection_classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(bboxes, (tf.shape(bboxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                preds, (tf.shape(preds)[0], -1, tf.shape(preds)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=0.45,
+            score_threshold=0.25
+        )
+        for i in range(batch_size):
+            for d in range(int(valid_detections[i])):
+                coco.submit_bbox_prediction(
+                    i,
+                    coco.convert_bbox_to_coco_order(detection_boxes[i][d] * shape[0], 1, 0, 3, 2),
+                    detection_scores[i][d],
+                    COCO_MAP[int(detection_classes[i][d]+1)]
+                )
+
+    dataset = COCODataset(batch_size, "RGB", "COCO_val2014_000000000000", images_path, anno_path,
+                          pre_processing="YOLO", sort_ascending=True)
+    runner = TFSavedModelRunner(model_path)
+
+    return run_model(run_single_pass, runner, dataset, batch_size, num_of_runs, timeout)
+
+
+def run_tflite_int8(model_path, batch_size, num_of_runs, timeout, images_path, anno_path):
+    def run_single_pass(tflite_runner, coco):
+        shape = (300, 300)
+        tflite_runner.set_input_tensor(tflite_runner.input_details[0]["index"], coco.get_input_array(shape))
+        tflite_runner.run()
+        detection_boxes = tflite_runner.get_output_tensor(tflite_runner.output_details[0]["index"])
+        detection_classes = tflite_runner.get_output_tensor(tflite_runner.output_details[1]["index"])
+        detection_classes += 1  # model uses indexing from 0 while COCO dateset start with idx of 1
+        detection_scores = tflite_runner.get_output_tensor(tflite_runner.output_details[2]["index"])
+        num_detections = tflite_runner.get_output_tensor(tflite_runner.output_details[3]["index"])
+        for i in range(batch_size):
+            for d in range(int(num_detections[i])):
+                coco.submit_bbox_prediction(
+                    i,
+                    coco.convert_bbox_to_coco_order(detection_boxes[i][d] * shape[0], 1, 0, 3, 2),
+                    detection_scores[i][d],
+                    int(detection_classes[i][d])
+                )
+
+    dataset = COCODataset(batch_size, "BGR", "COCO_val2014_000000000000", images_path, anno_path,
+                          pre_processing="SSD", sort_ascending=True)
+    runner = TFLiteRunner(model_path)
+
+    return run_model(run_single_pass, runner, dataset, batch_size, num_of_runs, timeout)
+
+
+def main():
+    args = parse_args()
+    if args.precision == "fp32":
+        run_tf_fp32(
+            args.model_path, args.batch_size, args.num_runs, args.timeout, args.images_path, args.anno_path
+        )
+    elif args.precision == "int8":
+        run_tflite_int8(
+            args.model_path, args.batch_size, args.num_runs, args.timeout, args.images_path, args.anno_path
+        )
+    else:
+        assert False
+
+
+if __name__ == "__main__":
+    main()
--- utils/tf.py
+++ utils/tf.py
@@ -1,7 +1,8 @@
 import os
 import time
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import utils.benchmark as bench_utils
+from tensorflow.python.saved_model import tag_constants
 
 
 class TFFrozenModelRunner:
@@ -17,7 +18,7 @@ class TFFrozenModelRunner:
         :param output_names: list of str, eg. ["detection_classes:0", "detection_boxes:0"]
         """
         self.__graph = self.__initialize_graph(path_to_model)
-        self.__sess = tf.Session(
+        self.__sess = tf.compat.v1.Session(
             config=self.__create_config(bench_utils.get_intra_op_parallelism_threads()),
             graph=self.__graph
         )
@@ -35,7 +36,7 @@ class TFFrozenModelRunner:
         :param inter_threads: int
         :return: TensorFlow config
         """
-        config = tf.ConfigProto()
+        config = tf.compat.v1.ConfigProto()
         config.allow_soft_placement = True
         config.intra_op_parallelism_threads = intra_threads
         config.inter_op_parallelism_threads = inter_threads
@@ -48,13 +49,13 @@ class TFFrozenModelRunner:
         :param path_to_model: str
         :return: TensorFlow graph
         """
-        graph = tf.Graph()
+        graph = tf.compat.v1.Graph()
         with graph.as_default():
-            graph_def = tf.GraphDef()
-            with tf.gfile.GFile(path_to_model, 'rb') as fid:
+            graph_def = tf.compat.v1.GraphDef()
+            with tf.compat.v1.gfile.GFile(path_to_model, 'rb') as fid:
                 serialized_graph = fid.read()
                 graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(graph_def, name="")
+                tf.compat.v1.import_graph_def(graph_def, name="")
         return graph
 
     def set_input_tensor(self, input_name: str, input_array):
@@ -90,4 +91,66 @@ class TFFrozenModelRunner:
         perf = bench_utils.print_performance_metrics(
             self.__warm_up_run_latency, self.__total_inference_time, self.__times_invoked, batch_size)
         self.__sess.close()
+        return perf
+
+
+class TFSavedModelRunner:
+    """
+    A class providing facilities to run TensorFlow frozen model (in frozen .pb format).
+    """
+    def __init__(self, path_to_weights: str):
+        """
+        A function initializing runner by providing path to model and list of output names (can be easily checked with
+        Netron app).
+
+        :param path_to_model: str, eg. "ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb"
+        :param output_names: list of str, eg. ["detection_classes:0", "detection_boxes:0"]
+        """
+        #self.__sess = tf.compat.v1.InteractiveSession(
+        #    config=self.__create_config(bench_utils.get_intra_op_parallelism_threads())
+        #)
+        self.__saved_model_loaded = tf.saved_model.load(path_to_weights, tags=[tag_constants.SERVING])
+        self.__model = self.__saved_model_loaded.signatures['serving_default']
+        self.__warm_up_run_latency = 0.0
+        self.__total_inference_time = 0.0
+        self.__times_invoked = 0
+
+    def __create_config(self, intra_threads: int, inter_threads=1):
+        """
+        A function creating TF config for given num of threads.
+
+        :param intra_threads: int
+        :param inter_threads: int
+        :return: TensorFlow config
+        """
+        config = tf.compat.v1.ConfigProto()
+        config.allow_soft_placement = True
+        config.intra_op_parallelism_threads = intra_threads
+        config.inter_op_parallelism_threads = inter_threads
+        return config
+
+    def run(self, input):
+        """
+        A function executing single pass over the network, measuring the time need and returning the output.
+
+        :return: dict, output dictionary with tensor names and corresponding output
+        """
+        start = time.time()
+        output = self.__model(tf.constant(input))
+        finish = time.time()
+        self.__total_inference_time += finish - start
+        if self.__times_invoked == 0:
+            self.__warm_up_run_latency += finish - start
+        self.__times_invoked += 1
+        return output
+
+    def print_performance_metrics(self, batch_size):
+        """
+        A function printing performance metrics on runs executed by the runner so far and then closing TF session.
+
+        :param batch_size: int, batch size - if batch size was varying over the runs an average should be supplied
+        """
+        perf = bench_utils.print_performance_metrics(
+            self.__warm_up_run_latency, self.__total_inference_time, self.__times_invoked, batch_size)
+        #self.__sess.close()
         return perf
