ad1e3ec77c5fa32ec07450e7c589a3da9a23d7dd
Jan Grzybek <52271793+jangrzybek@users.noreply.github.com>
2021-05-06 17:12:13 +0200

    YOLO v4 tiny + TF SavedModel runner (#22)
    * YOLO v4 tiny in fp32 precision (SavedModel TF)
    * TF SavedModel runner class
    * COCO category translator function in COCO dataset class

--- utils/pre_processing.py
+++ utils/pre_processing.py
@@ -13,6 +13,8 @@ def pre_process(input_array, pre_processing_approach: str, color_model=None):
     """
     if pre_processing_approach == "SSD":
         return pre_process_ssd(input_array)
+    if pre_processing_approach == "YOLO":
+        return pre_process_yolo(input_array)
     if pre_processing_approach == "VGG":
         return pre_process_vgg(input_array, color_model)
     if pre_processing_approach == "Inception":
@@ -36,6 +38,21 @@ def pre_process_ssd(input_array):
     # kinda equivalent solution:
     # input_array -= 127.5
     # input_array *= 0.007843
+    return input_array
+
+
+def pre_process_yolo(input_array):
+    """
+    A function pre-processing an input array in the way expected by some YOLO models.
+
+    Values are converted from int 0 <-> 255 range to float range of 0.0 <-> 1.0. (IN RATHER DIRECT WAY)
+
+    :param input_array: numpy array containing image data
+    :return: numpy array containing pre-processed image data
+    """
+    input_array = input_array.astype("float32")
+
+    input_array /= 255.0
     return input_array
 
 
--- utils/tf.py
+++ utils/tf.py
@@ -1,7 +1,8 @@
 import os
 import time
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import utils.benchmark as bench_utils
+from tensorflow.python.saved_model import tag_constants
 
 
 class TFFrozenModelRunner:
@@ -17,7 +18,7 @@ class TFFrozenModelRunner:
         :param output_names: list of str, eg. ["detection_classes:0", "detection_boxes:0"]
         """
         self.__graph = self.__initialize_graph(path_to_model)
-        self.__sess = tf.Session(
+        self.__sess = tf.compat.v1.Session(
             config=self.__create_config(bench_utils.get_intra_op_parallelism_threads()),
             graph=self.__graph
         )
@@ -35,7 +36,7 @@ class TFFrozenModelRunner:
         :param inter_threads: int
         :return: TensorFlow config
         """
-        config = tf.ConfigProto()
+        config = tf.compat.v1.ConfigProto()
         config.allow_soft_placement = True
         config.intra_op_parallelism_threads = intra_threads
         config.inter_op_parallelism_threads = inter_threads
@@ -48,13 +49,13 @@ class TFFrozenModelRunner:
         :param path_to_model: str
         :return: TensorFlow graph
         """
-        graph = tf.Graph()
+        graph = tf.compat.v1.Graph()
         with graph.as_default():
-            graph_def = tf.GraphDef()
-            with tf.gfile.GFile(path_to_model, 'rb') as fid:
+            graph_def = tf.compat.v1.GraphDef()
+            with tf.compat.v1.gfile.GFile(path_to_model, 'rb') as fid:
                 serialized_graph = fid.read()
                 graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(graph_def, name="")
+                tf.compat.v1.import_graph_def(graph_def, name="")
         return graph
 
     def set_input_tensor(self, input_name: str, input_array):
@@ -68,7 +69,7 @@ class TFFrozenModelRunner:
 
     def run(self):
         """
-        A function executing single pass over the network, measuring the time need and returning the output.
+        A function executing single pass over the network, measuring the time needed and returning the output.
 
         :return: dict, output dictionary with tensor names and corresponding output
         """
@@ -90,4 +91,49 @@ class TFFrozenModelRunner:
         perf = bench_utils.print_performance_metrics(
             self.__warm_up_run_latency, self.__total_inference_time, self.__times_invoked, batch_size)
         self.__sess.close()
+        return perf
+
+
+class TFSavedModelRunner:
+    """
+    A class providing facilities to run TensorFlow saved model (in SavedModel format).
+    """
+    def __init__(self, path_to_model: str):
+        """
+        A function initializing runner by providing path to model directory.
+
+        :param path_to_model: str, eg. "./ugabuga/yolo_saved_model/"
+        """
+        tf.config.threading.set_intra_op_parallelism_threads(bench_utils.get_intra_op_parallelism_threads())
+        tf.config.threading.set_inter_op_parallelism_threads(1)
+        self.__saved_model_loaded = tf.saved_model.load(path_to_model, tags=[tag_constants.SERVING])
+        self.__model = self.__saved_model_loaded.signatures['serving_default']
+        self.__warm_up_run_latency = 0.0
+        self.__total_inference_time = 0.0
+        self.__times_invoked = 0
+
+    def run(self, input):
+        """
+        A function assigning values to input tensor, executing single pass over the network, measuring the time needed
+        and finally returning the output.
+
+        :return: dict, output dictionary with tensor names and corresponding output
+        """
+        start = time.time()
+        output = self.__model(input)
+        finish = time.time()
+        self.__total_inference_time += finish - start
+        if self.__times_invoked == 0:
+            self.__warm_up_run_latency += finish - start
+        self.__times_invoked += 1
+        return output
+
+    def print_performance_metrics(self, batch_size):
+        """
+        A function printing performance metrics on runs executed by the runner so far.
+
+        :param batch_size: int, batch size - if batch size was varying over the runs an average should be supplied
+        """
+        perf = bench_utils.print_performance_metrics(
+            self.__warm_up_run_latency, self.__total_inference_time, self.__times_invoked, batch_size)
         return perf
