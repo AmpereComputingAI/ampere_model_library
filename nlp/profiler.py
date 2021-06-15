1a38ea39a6e3718f18927c6118341fa8c46fec55
MarcelWilnicki <marcel@onspecta.com>
2021-06-15 19:24:42 +0200

    wip

--- nlp/profiler.py
+++ nlp/profiler.py
@@ -4,4 +4,9 @@ import json
 with gzip.open("../logs/plugins/profile/2021_06_15_15_34_43/49524a0fc1dd.trace.json.gz", "r") as f:
     data = f.read()
     j = json.loads(data.decode('utf-8'))
-    print(j)
+    app_json = json.dumps(j)
+
+test
+f = open("demofile2.txt", "w")
+f.write(app_json)
+f.close()
