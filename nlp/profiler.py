4a059bbc3f3e5757ca56ba4361a2f27bd42406df
MarcelWilnicki <marcel@onspecta.com>
2021-06-24 13:38:51 +0200

    wip

--- nlp/profiler.py
+++ nlp/profiler.py
@@ -1,6 +1,7 @@
 import gzip
 import json
 import os
+import csv
 
 from collections import Counter
 
@@ -10,18 +11,22 @@ def print_profiler_results(path_to_logs: str):
     for root, dirs, _ in os.walk(path_to_logs + '/plugins/profile/'):
         for d in dirs:
             directory = os.path.join(root, d)
+            print('TTETEETETET')
+            print(directory)
 
     my_items = os.listdir(directory + '/')
     print(any(".trace.json.gz" in s for s in my_items))
 
     matching = [s for s in my_items if ".trace.json.gz" in s]
 
+    print(directory)
     with gzip.open(directory + '/' + matching[0], "r") as f:
         data = f.read()
         j = json.loads(data.decode('utf-8'))
 
-        microToMinutes = 1.66666667 * 10 ** (-8)
-        microToMilliseconds = 0.001
+        micro_to_minutes = 1.66666667 * 10 ** (-8)
+        micro_to_milliseconds = 0.001
+        micro_to_seconds = 0.000001
         my_dict = {}
         occurrences = []
 
@@ -40,24 +45,39 @@ def print_profiler_results(path_to_logs: str):
             else:
                 continue
 
+        f = open(path_to_logs + '/output_data.csv', 'w')
+        writer = csv.writer(f)
+
+        writer.writerow(['name', 'time in milliseconds', 'ratio', 'occurrences'])
+
         total = 0.0
         print(my_dict)
 
         # total time of running the model
+        # print(runtimes)
+        # sorted_runtimes = sorted(runtimes)
+        # print(sorted_runtimes)
         print("-" * 25, "TOTAL TIME", '-' * 25, '\n')
 
         for value in my_dict.values():
             total += value
-        print(f'total time in minutes: {total * microToMinutes}')
+        print(f'total time in seconds per inference: {total * micro_to_seconds}')
         print('\n' * 3)
 
         occurrences_counter = Counter(occurrences)
         # ratio and total time par layer
         print("-" * 25, "RATIOS AND TIME PER LAYER", '-' * 25, '\n')
         for pair in my_dict.items():
-            print(f'for {pair[0]} total time in milliseconds is: {pair[1] * microToMilliseconds}')
+            in_milliseconds = pair[1] * micro_to_milliseconds
+            ratio = pair[1] / total
+            occurrences = occurrences_counter.get(pair[0])
+            print(f'for {pair[0]} total time in milliseconds is: {in_milliseconds}')
             print(f'for {pair[0]} ratio to total time is: {pair[1] / total}')
-            print(f'{pair[0]} has occurred {occurrences_counter.get(pair[0])} times')
+            print(f'{pair[0]} has occurred {occurrences} times')
             print('*' * 45)
+            row = [pair[0], in_milliseconds, ratio, occurrences]
+            writer.writerow(row)
         print('\n' * 3)
+
+        f.close()
 
--- nlp/run.py
+++ nlp/run.py
@@ -8,10 +8,14 @@ import argparse
 import utils.misc as utils
 from profiler import print_profiler_results
 import shutil
+import csv
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Run bert-base-uncased")
+    parser = argparse.ArgumentParser(description="Benchmark NLP models")
+    parser.add_argument("-m", "--model",
+                        type=str, default='bert-base-uncased',
+                        help="batch size to feed the model with")
     parser.add_argument("-b", "--batch_size",
                         type=int, default=8,
                         help="batch size to feed the model with")
@@ -20,40 +24,49 @@ def parse_args():
                         help="sequence length to feed the model with")
     parser.add_argument("--profiler",
                         action="store_const", const=True,
-                        help="sequence length to feed the model with")
+                        help="Run the model with Tensorflow profiler")
+    parser.add_argument("--precision",
+                        type=str, default="fp32", choices=["fp32", "fp16"],
+                        help="precision of the model")
     return parser.parse_args()
 
 
-def benchmark_bert_base_uncased(batch_size, sequence_length, profiler):
+def benchmark_nlp_model(model, batch_size, sequence_length, profiler, precision):
 
-    # if profiler or os.environ['DLS_PROFILER'] == '1':
-    if profiler or 'DLS_PROFILER' in os.environ and os.environ['DLS_PROFILER'] == '1':
+    print(profiler)
+    if precision == "fp32":
+        fp16 = False
+    else:
+        fp16 = True
+
+    if profiler or os.environ.get('DLS_PROFILER', "0") == "1":
         os.environ["PROFILER"] = "1"
 
-        # set the logs
         env_var = "PROFILER_LOG_DIR"
         logs_dir = utils.get_env_variable(
             env_var, f"Path to profiler log directory has not been specified with {env_var} flag")
+
+        # set the logs
         # remove old logs from logs directory
         try:
             shutil.rmtree(logs_dir + '/plugins/profile')
         except FileNotFoundError:
             print('no logs to clear, moving on ...')
 
-    args = TensorFlowBenchmarkArguments(models=["bert-base-uncased"], batch_sizes=[batch_size],
-                                        sequence_lengths=[sequence_length], inference=True, memory=False)
+    # if profiler or os.environ['DLS_PROFILER'] == '1':
+    args = TensorFlowBenchmarkArguments(models=[model], batch_sizes=[batch_size],
+                                        sequence_lengths=[sequence_length], inference=True, memory=False,
+                                        fp16=fp16)
 
     benchmark = TensorFlowBenchmark(args)
     results = benchmark.run()
-    print(results)
 
-    if 'PROFILER' in os.environ:
-        print_profiler_results(logs_dir)
+    print(results)
 
 
 def main():
     args = parse_args()
-    benchmark_bert_base_uncased(args.batch_size, args.sequence_length, args.profiler)
+    benchmark_nlp_model(args.model, args.batch_size, args.sequence_length, args.profiler, args.precision)
 
 
 if __name__ == "__main__":
