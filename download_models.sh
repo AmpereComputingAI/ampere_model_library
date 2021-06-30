1afc4e82fa8605034fdf505b5ee041121e829816
Marcel Wilnicki <42609544+MarcelWilnicki@users.noreply.github.com>
2021-06-30 12:09:08 +0200

    add resnet v2 101 run script, README, update download_models.sh (#36)

--- download_models.sh
+++ download_models.sh
@@ -43,6 +43,10 @@ tar -xvf classification/resnet_50_v2/resnet_50_v2_tf_fp32.tar.gz classification/
 rm classification/resnet_50_v2/resnet_50_v2_tf_fp32.tar.gz
 wget -O classification/resnet_50_v2/resnet_50_v2_tflite_int8.tflite https://www.dropbox.com/s/igfber3q86yq1bh/resnet_50_v2_quant.tflite
 
+# resnet_v2_101
+wget -O classification/resnet_v2_101/resnet_v2_101_tf_fp32.pb https://www.dropbox.com/s/ckojofwnj30ouhn/resnet_v2_101_tf_fp32.pb
+wget -O classification/resnet_v2_101/resnet_v2_101_tflite_int8.tflite https://www.dropbox.com/s/n5sw2povn7cmdio/resnet_v2_101_tflite_int8.tflite
+
 # inception v3
 wget -O classification/inception_v3/inception_v3_tf_fp32.pb https://www.dropbox.com/s/ccfmzojpo3v90bv/inception_v3_tf_fp32.pb
 wget -O classification/inception_v3/inception_v3_tflite_int8.tflite https://www.dropbox.com/s/wtq3gix7lhyef6t/inception_v3_tflite_int8.tflite
