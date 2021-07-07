d09244c08d033a7548903adf54cc1d40143ad67b
Jerzy Baranowski <jerry@onspecta.com>
2021-07-07 14:08:42 +0000

    nlp processing update

--- nlp/profiler.py
+++ nlp/profiler.py
@@ -4,6 +4,8 @@ import os
 import csv
 
 from collections import Counter
+from rich.console import Console
+from rich.table import Table
 
 
 def print_profiler_results(path_to_logs: str):
@@ -11,15 +13,18 @@ def print_profiler_results(path_to_logs: str):
     for root, dirs, _ in os.walk(path_to_logs + '/plugins/profile/'):
         for d in dirs:
             directory = os.path.join(root, d)
-            print('TTETEETETET')
-            print(directory)
+
 
     my_items = os.listdir(directory + '/')
-    print(any(".trace.json.gz" in s for s in my_items))
 
     matching = [s for s in my_items if ".trace.json.gz" in s]
 
-    print(directory)
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("CSV log file stored under adress:", width = 80, justify = "center")
+    table.add_row(directory)
+    console.print(table)
+
     with gzip.open(directory + '/' + matching[0], "r") as f:
         data = f.read()
         j = json.loads(data.decode('utf-8'))
@@ -51,32 +56,33 @@ def print_profiler_results(path_to_logs: str):
         writer.writerow(['name', 'time in milliseconds', 'ratio', 'occurrences'])
 
         total = 0.0
-        print(my_dict)
-
-        # total time of running the model
-        # print(runtimes)
-        # sorted_runtimes = sorted(runtimes)
-        # print(sorted_runtimes)
-        print("-" * 25, "TOTAL TIME", '-' * 25, '\n')
-
         for value in my_dict.values():
             total += value
-        print(f'total time in seconds per inference: {total * micro_to_seconds}')
-        print('\n' * 3)
 
-        occurrences_counter = Counter(occurrences)
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("TOTAL TIME", width = 80, justify = "center")
+        table.add_row("Total time of execution in seconds")
+        table.add_row(str(total * micro_to_seconds))
+        console.print(table)
+   
+
+        res_table = Table(show_header=True, header_style="bold magenta", title = "Profiler results")
+        res_table.add_column("Layer", width = 30)
+        res_table.add_column("Total time [ms]", width = 15)
+        res_table.add_column("Ratio of total time [%]", width = 15)
+        res_table.add_column("Number of occurences", width = 10)
+       
         # ratio and total time par layer
-        print("-" * 25, "RATIOS AND TIME PER LAYER", '-' * 25, '\n')
+        occurrences_counter = Counter(occurrences)
         for pair in my_dict.items():
             in_milliseconds = pair[1] * micro_to_milliseconds
             ratio = pair[1] / total
             occurrences = occurrences_counter.get(pair[0])
-            print(f'for {pair[0]} total time in milliseconds is: {in_milliseconds}')
-            print(f'for {pair[0]} ratio to total time is: {pair[1] / total}')
-            print(f'{pair[0]} has occurred {occurrences} times')
-            print('*' * 45)
+            ratio = pair[1] / total
+            res_table.add_row(pair[0], "{:.4f}".format(in_milliseconds), "{:.3f}".format(ratio), str(occurrences))
             row = [pair[0], in_milliseconds, ratio, occurrences]
             writer.writerow(row)
+        console.print(res_table)    
         print('\n' * 3)
 
         f.close()
