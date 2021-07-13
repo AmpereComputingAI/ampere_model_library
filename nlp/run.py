6ef4bf50c665be8e2bea17fcbeea5c4dd2a9b123
marcel <marcel.wilnicki@gmail.com>
2021-07-13 14:16:59 +0000

    wip

--- nlp/run.py
+++ nlp/run.py
@@ -4,13 +4,13 @@ import tensorflow as tf
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Benchmark NLP models")
-    parser.add_argument("-m", "--model", type=str, default="bert-base-uncased", required=False,
+    parser.add_argument("-m", "--model", nargs='+', type=str, required=True,
                         help="you can see the available list of models here: "
                         "https://huggingface.co/transformers/pretrained_models.html")
-    parser.add_argument("-b", "--batch_size",
-                        type=int, default=8, required=False,
+    parser.add_argument("-b", "--batch_size", default=8,
+                        type=int, required=False,
                         help="batch size to feed the model with")
-    parser.add_argument("--sequence_length", required=False,
+    parser.add_argument("--sequence_length", nargs='+', required=False,
                         type=int, default=8,
                         help="sequence length to feed the model with")
     parser.add_argument("--intra", required=False,
@@ -21,11 +21,14 @@ def parse_args():
 
 def benchmark_nlp_model(model, batch_size, sequence_length, intra):
 
+    print(sequence_length)
+    print(model)
+
     tf.config.threading.set_intra_op_parallelism_threads(intra)
     tf.config.threading.set_inter_op_parallelism_threads(1)
 
-    args = TensorFlowBenchmarkArguments(models=[model], batch_sizes=[batch_size],
-                                        sequence_lengths=[sequence_length], memory=True)
+    args = TensorFlowBenchmarkArguments(models=model, batch_sizes=[batch_size],
+                                        sequence_lengths=sequence_length, memory=False, inference_time_csv_file=True)
 
     benchmark = TensorFlowBenchmark(args)
     results = benchmark.run()
