ca0f29cb276244d6878c4d8b89d8006d6700ad13
MarcelWilnicki <marcel@onspecta.com>
2021-07-15 01:32:42 +0200

    wip

--- nlp/profiler.py
+++ nlp/profiler.py
@@ -12,6 +12,7 @@ from rich.table import Table
 micro_to_minutes = 1.66666667 * 10 ** (-8)
 micro_to_milliseconds = 0.001
 micro_to_seconds = 0.000001
+pd.set_option('display.max_rows', 500)
 
 
 def calculate_total_time(events_df):
@@ -19,17 +20,31 @@ def calculate_total_time(events_df):
     end = -1
     
     for index, row in events_df.iterrows():
+        print('start')
+        print(row["Start"])
+        print('duraton')
+        print(row["Duration"])
+
+        print('func_end_time')
         func_end_time = row["Start"] + row["Duration"]
+        print(func_end_time)
+
+        print('end')
+        print(end)
         if func_end_time > end:
             end = func_end_time
+
+    print(end)
     return end
+
 
 def variable_read_op_time(events_df):
     return events_df.loc[events_df['Op_Name'] == "ReadVariableOp"]["Duration"].sum()
 
+
 def print_profiler_results_dls_only(dls_opts_df, total):
     console = Console()
-    res_table = Table(show_header=True, header_style="bold magenta", title = "TF Profiler results - DLS ops")
+    res_table = Table(show_header=True, header_style="bold magenta", title="TF Profiler results - DLS ops")
     res_table.add_column("DLS Layer", width = 15)
     res_table.add_column("Total time ms", width = 10)
     res_table.add_column("Ratio of total time %", width = 5)
@@ -43,49 +58,97 @@ def print_profiler_results_dls_only(dls_opts_df, total):
         res_table.add_row(name, "{:.4f}".format(in_milliseconds), "{:.2f}".format(ratio), row["Shape"], str(row["Occurences"]))
     console.print(res_table)
 
+
 def print_profiler_results_all(events_df, total):
     console = Console()
-    res_table = Table(show_header=True, header_style="bold magenta", title = "TF Profiler results - All ops")
-    res_table.add_column("DLS Layer", width = 15)
-    res_table.add_column("Total time ms", width = 10)
-    res_table.add_column("Ratio of total time %", width = 5)
-    res_table.add_column("Shape", width = 70)
-    res_table.add_column("Number of occurences", width = 5)
+    res_table = Table(show_header=True, header_style="bold magenta", title="TF Profiler results - All ops")
+    res_table.add_column("DLS Layer", width=45)
+    res_table.add_column("Total time ms", width=20)
+    res_table.add_column("Ratio of total time %", width=10)
+    res_table.add_column("Shape", width=35)
+    res_table.add_column("Number of occurrences", width=5)
        
     for index, row in events_df.iterrows():
         name = row["Op_Name"].replace(":_DLSSuperNode", '').replace("DLS-SN-", '')
         in_milliseconds = row["Duration"] * micro_to_milliseconds
         ratio = row["Duration"] / total * 100
         res_table.add_row(name, "{:.4f}".format(in_milliseconds), "{:.2f}".format(ratio), row["Shape"], str(row["Occurences"]))
-    console.print(res_table)    
+    console.print(res_table)
+
+
+def print_profiler_results_all_by_occurrences(events_df, total):
+    console = Console()
+    res_table = Table(show_header=True, header_style="bold magenta", title="TF Profiler results - All ops")
+    res_table.add_column("Layer", width=45)
+    res_table.add_column("Total time ms", width=20)
+    res_table.add_column("Ratio of total time %", width=10)
+    res_table.add_column("Number of occurrences", width=5)
+
+    my_dict = {}
+
+    df = events_df.groupby('Op_Name').Duration.sum()
+    # df1 = events_df.groupby('Op_Name')['Op_Name'].nunique()
+
+    df1 = events_df.Op_Name.value_counts()
+
+    test = pd.concat([df, df1], axis=1).reset_index()
+
+    df_new = test.rename(columns={'index': 'Op_Name', 'Op_Name': 'Occurrences'})
+
+    tetete = df_new.sort_values(by=['Occurrences'], ascending=False)
+
+    # df2 = pd.merge(df, df1, how="left", on="Op_Name")
+    # print(type(df))
+    # print(type(df1))
+    # print(type(test))
+    #
+    # print(df)
+    # print(df1)
+    # print(test)
+    # print(df_new)
+    # print(tetete)
+
+    print((tetete.Duration.sum()) * micro_to_seconds)
+
+    # print(df2)
+    # if 'name' in j["traceEvents"][i] and j["traceEvents"][i].get("name") not in my_dict.keys():
+
+    for index, row in tetete.iterrows():
+        name = row["Op_Name"]
+        duration = row["Duration"] * micro_to_milliseconds
+        occurrences = row['Occurrences']
+        res_table.add_row(name, "{:.4f}".format(duration), str(occurrences))
+    console.print(res_table)
+
 
 def print_total_vs_dls_time(total, dls_time_sum, variable_read_ops):
     console = Console()
     table = Table(show_header=True, header_style="bold magenta")
-    table.add_column("Total time", width = 45, justify = "center")
-    table.add_column("DLS time", width = 45, justify = "center")
-    table.add_column("DLS to Total ratio %", width = 8, justify = "right")
-    table.add_column("DLS to Total ratio excle varReadOP %", width = 11, justify = "right")
+    table.add_column("Total time", width=45, justify="center")
+    table.add_column("DLS time", width=45, justify="center")
+    table.add_column("DLS to Total ratio %", width=8, justify="right")
+    table.add_column("DLS to Total ratio excle varReadOP %", width=11, justify="right")
     ratio = dls_time_sum / total * 100
     ratio_excl_varReadOP = dls_time_sum / (total - variable_read_ops) * 100
     table.add_row("{:.5f}".format(total * micro_to_seconds), "{:.5f}".format(dls_time_sum * micro_to_seconds), "{:.2f}".format(ratio), "{:.2f}".format(ratio_excl_varReadOP))
     console.print(table)
 
+
 def print_csv_logfile_location(directory):
     console = Console()
     table = Table(show_header=True, header_style="bold magenta")
-    table.add_column("CSV log file stored under adress:", width = 118, justify = "center")
+    table.add_column("CSV log file stored under address:", width=118, justify="center")
     table.add_row(directory)
     console.print(table)
+
 
 def generate_otput_csv_file(events_df, dls_opts_df, path_to_logs):
         f = open(path_to_logs + '/output_data.csv', 'w')
         writer = csv.writer(f)
         writer.writerow(['name', 'time in milliseconds', 'ratio', 'occurrences'])
         # events_df.to_csv(path_to_logs + '/output_data_df.csv', index = False)
-        #TODO MARCEL add buisness logic
+        # TODO MARCEL add business logic
         f.close()
-
 
 
 def print_profiler_results(path_to_logs: str):
@@ -105,7 +168,7 @@ def print_profiler_results(path_to_logs: str):
         json_data = json.loads(data.decode('utf-8'))
 
         data_columns = ["Op_Name", "Start", "Duration", "Occurences", "Shape", "Is_DLS_Op"]
-        events_df = pd.DataFrame(columns = data_columns)
+        events_df = pd.DataFrame(columns=data_columns)
 
         for i in range(len(json_data["traceEvents"])):
             name = ""
@@ -138,19 +201,19 @@ def print_profiler_results(path_to_logs: str):
                     shape_str = json_data["traceEvents"][i]['args'].get("shape")
                     if bool(shape_str.strip().replace('(', '').replace(')', '')):
                         shape = shape_str
-            events_df = events_df.append(pd.DataFrame([[name, start, duration, occurences, shape, is_DLS_Op]], columns = data_columns), ignore_index= True)
+            events_df = events_df.append(pd.DataFrame([[name, start, duration, occurences, shape, is_DLS_Op]],
+                                                      columns=data_columns), ignore_index=True)
         
-        events_df = events_df.sort_values(by="Duration", ascending = False)
+        events_df = events_df.sort_values(by="Duration", ascending=False)
         total = calculate_total_time(events_df)
 
         dls_opts_df = events_df[events_df["Is_DLS_Op"] == True]
-        dls_opts_df = dls_opts_df.sort_values(by = "Duration", ascending = False)
+        dls_opts_df = dls_opts_df.sort_values(by="Duration", ascending=False)
         dls_time_sum = dls_opts_df["Duration"].sum()
 
         var_read_op = variable_read_op_time(events_df)
         print_total_vs_dls_time(total, dls_time_sum, var_read_op)
-        print_profiler_results_dls_only(dls_opts_df, total)
+        # print_profiler_results_dls_only(dls_opts_df, total)
         # print_profiler_results_all(events_df, total)
+        print_profiler_results_all_by_occurrences(events_df, total)
         generate_otput_csv_file(events_df, dls_opts_df, path_to_logs)
-       
-
