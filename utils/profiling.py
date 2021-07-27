e1a45fdb73b328f6585405e7e7228d36c6688ce6
MarcelWilnicki <marcel@onspecta.com>
2021-07-27 17:48:17 +0200

    wip

--- utils/profiling.py
+++ utils/profiling.py
@@ -1,21 +1,17 @@
 import os
-from datetime import datetime
-import pathlib
-from natural_language_processing.huggingface.profiler_results.utils import get_profiler_results_path
-
-
-# ======================================================================================================================
-
+import csv
 import gzip
 import json
-import os
-import csv
+import pathlib
 import numpy as np
 import pandas as pd
+from datetime import datetime
 
 from collections import Counter
 from rich.console import Console
 from rich.table import Table
+
+from natural_language_processing.huggingface.profiler_results.utils import get_profiler_results_path
 
 micro_to_minutes = 1.66666667 * 10 ** (-8)
 micro_to_milliseconds = 0.001
@@ -48,14 +44,14 @@ def print_profiler_results_dls_only(dls_opts_df, total):
     res_table.add_column("Total time ms", width=10)
     res_table.add_column("Ratio of total time %", width=5)
     res_table.add_column("Shape", width=70)
-    res_table.add_column("Number of occurences", width=5)
+    res_table.add_column("Number of occurrences", width=5)
 
     for index, row in dls_opts_df.iterrows():
         name = row["Op_Name"].replace(":_DLSSuperNode", '').replace("DLS-SN-", '')
         in_milliseconds = row["Duration"] * micro_to_milliseconds
         ratio = row["Duration"] / total * 100
         res_table.add_row(name, "{:.4f}".format(in_milliseconds), "{:.2f}".format(ratio), row["Shape"],
-                          str(row["Occurences"]))
+                          str(row["occurrences"]))
     console.print(res_table)
 
 
@@ -73,31 +69,62 @@ def print_profiler_results_all(events_df, total):
         in_milliseconds = row["Duration"] * micro_to_milliseconds
         ratio = row["Duration"] / total * 100
         res_table.add_row(name, "{:.4f}".format(in_milliseconds), "{:.2f}".format(ratio), row["Shape"],
-                          str(row["Occurences"]))
+                          str(row["occurrences"]))
     console.print(res_table)
 
 
 # still under development
-def print_profiler_results_all_by_occurrences(events_df, total):
-    console = Console()
-    res_table = Table(show_header=True, header_style="bold magenta", title="TF Profiler results - All ops")
-    res_table.add_column("Layer", width=45)
-    res_table.add_column("Total time ms", width=20)
-    res_table.add_column("Ratio of total time %", width=10)
-    res_table.add_column("Number of occurrences", width=5)
+# def print_profiler_results_all_by_occurrences(events_df, total):
+#     console = Console()
+#     res_table = Table(show_header=True, header_style="bold magenta", title="TF Profiler results - All ops")
+#     res_table.add_column("Layer", width=45)
+#     res_table.add_column("Total time ms", width=20)
+#     res_table.add_column("Ratio of total time %", width=10)
+#     res_table.add_column("Number of occurrences", width=5)
 
-    df = events_df.groupby('Op_Name').Duration.sum()
-    df1 = events_df.Op_Name.value_counts()
-    test = pd.concat([df, df1], axis=1).reset_index()
-    df_new = test.rename(columns={'index': 'Op_Name', 'Op_Name': 'Occurrences'})
-    tetete = df_new.sort_values(by=['Occurrences'], ascending=False)
+    # df = events_df.groupby('Op_Name').Duration.sum()
+    # df1 = events_df.Op_Name.value_counts()
+    # test = pd.concat([df, df1], axis=1).reset_index()
+    # df_new = test.rename(columns={'index': 'Op_Name', 'Op_Name': 'Occurrences'})
+    # tetete = df_new.sort_values(by=['Occurrences'], ascending=False)
+    #
+    # for index, row in tetete.iterrows():
+    #     name = row["Op_Name"]
+    #     duration = row["Duration"] * micro_to_milliseconds
+    #     occurrences = row['Occurrences']
+    #     res_table.add_row(name, "{:.4f}".format(duration), str(occurrences))
 
-    for index, row in tetete.iterrows():
-        name = row["Op_Name"]
-        duration = row["Duration"] * micro_to_milliseconds
-        occurrences = row['Occurrences']
-        res_table.add_row(name, "{:.4f}".format(duration), str(occurrences))
-    console.print(res_table)
+    # print('-' * 85)
+    #
+    # unique = events_df.Op_Name.unique()
+    # unique_operations_duration = {}
+    # # create dictionary with durations of specific operations
+    # for i in range(unique.shape[0]):
+    #     unique_operations_duration[unique[i]] = 0.0
+    #
+    # # print(unique_operations_duration)
+    #
+    # events_df = events_df.sort_values(by="Start")
+    # end = -1
+    #
+    # for index, row in events_df.iterrows():
+
+        # print(row["Op_Name"])
+        # print(row["Start"])
+        # print(row["Duration"])
+
+    #     func_end_time = row["Start"] + row["Duration"]
+    #
+    #     unique_operations_duration[row["Op_Name"]] += row["Duration"]
+    #     if func_end_time > end:
+    #         end = func_end_time
+    #
+    # print(end)
+    #
+    # console.print(res_table)
+    #
+    # print(unique_operations_duration)
+    # print('sum: ', sum(unique_operations_duration.values()))
 
 
 def print_total_vs_dls_time(total, dls_time_sum, variable_read_ops):
@@ -146,8 +173,10 @@ def print_profiler_results(path_to_logs: str):
         data = f.read()
         json_data = json.loads(data.decode('utf-8'))
 
-        data_columns = ["Op_Name", "Start", "Duration", "Occurences", "Shape", "Is_DLS_Op"]
+        data_columns = ["Op_Name", "Start", "Duration", "occurrences", "Shape", "Is_DLS_Op"]
         events_df = pd.DataFrame(columns=data_columns)
+
+        # unique_operations = {}
 
         for i in range(len(json_data["traceEvents"])):
             name = ""
@@ -155,19 +184,23 @@ def print_profiler_results(path_to_logs: str):
             duration = -1.0
             shape = str("")
             is_DLS_Op = False
-            occurences = 0
+            occurrences = 0
+
+            # add key, value pair to dictionary
+            # if 'name' in json_data["traceEvents"][i] and json_data["traceEvents"][i].get("name") not in unique_operations.keys():
+            #     unique_operations[json_data["traceEvents"][i].get("name")] = 0.0
 
             if 'name' in json_data["traceEvents"][i]:
                 tmp_name = json_data["traceEvents"][i].get("name")
                 if tmp_name not in events_df["Op_Name"]:
                     name = tmp_name
-                    occurences = 1
+                    occurrences = 1
                     if json_data["traceEvents"][i]["name"].find("DLS") != -1:
                         is_DLS_Op = True
                 else:
-                    occurences_val = events_df.loc[events_df['Op_Name'] == tmp_name]["occurences"]
-                    occurences_val = occurences_val + 1
-                    events_df.loc[events_df['Op_Name'] == tmp_name]["occurences"] = occurences_val
+                    occurrences_val = events_df.loc[events_df['Op_Name'] == tmp_name]["occurrences"]
+                    occurrences_val = occurrences_val + 1
+                    events_df.loc[events_df['Op_Name'] == tmp_name]["occurrences"] = occurrences_val
 
             if 'dur' in json_data["traceEvents"][i].keys():
                 duration = json_data["traceEvents"][i].get("dur")
@@ -180,7 +213,7 @@ def print_profiler_results(path_to_logs: str):
                     shape_str = json_data["traceEvents"][i]['args'].get("shape")
                     if bool(shape_str.strip().replace('(', '').replace(')', '')):
                         shape = shape_str
-            events_df = events_df.append(pd.DataFrame([[name, start, duration, occurences, shape, is_DLS_Op]],
+            events_df = events_df.append(pd.DataFrame([[name, start, duration, occurrences, shape, is_DLS_Op]],
                                                       columns=data_columns), ignore_index=True)
 
         events_df = events_df.sort_values(by="Duration", ascending=False)
@@ -192,11 +225,11 @@ def print_profiler_results(path_to_logs: str):
 
         var_read_op = variable_read_op_time(events_df)
         print_total_vs_dls_time(total, dls_time_sum, var_read_op)
-        print_profiler_results_dls_only(dls_opts_df, total)
-        # print_profiler_results_all(events_df, total)
-        # print_profiler_results_all_by_occurrences(events_df, total)
+        # print_profiler_results_dls_only(dls_opts_df, total)
+        print_profiler_results_all(events_df, total)
         generate_otput_csv_file(events_df, dls_opts_df, path_to_logs)
-# ======================================================================================================================
+
+        # print(unique_operations)
 
 
 profile_path = None
