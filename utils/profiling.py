a36c6251509c4932e4acb87f2d22ae3c0120afcb
MarcelWilnicki <marcel@onspecta.com>
2021-07-27 18:43:42 +0200

    refactor profiler code

--- utils/profiling.py
+++ utils/profiling.py
@@ -73,60 +73,6 @@ def print_profiler_results_all(events_df, total):
     console.print(res_table)
 
 
-# still under development
-# def print_profiler_results_all_by_occurrences(events_df, total):
-#     console = Console()
-#     res_table = Table(show_header=True, header_style="bold magenta", title="TF Profiler results - All ops")
-#     res_table.add_column("Layer", width=45)
-#     res_table.add_column("Total time ms", width=20)
-#     res_table.add_column("Ratio of total time %", width=10)
-#     res_table.add_column("Number of occurrences", width=5)
-
-    # df = events_df.groupby('Op_Name').Duration.sum()
-    # df1 = events_df.Op_Name.value_counts()
-    # test = pd.concat([df, df1], axis=1).reset_index()
-    # df_new = test.rename(columns={'index': 'Op_Name', 'Op_Name': 'Occurrences'})
-    # tetete = df_new.sort_values(by=['Occurrences'], ascending=False)
-    #
-    # for index, row in tetete.iterrows():
-    #     name = row["Op_Name"]
-    #     duration = row["Duration"] * micro_to_milliseconds
-    #     occurrences = row['Occurrences']
-    #     res_table.add_row(name, "{:.4f}".format(duration), str(occurrences))
-
-    # print('-' * 85)
-    #
-    # unique = events_df.Op_Name.unique()
-    # unique_operations_duration = {}
-    # # create dictionary with durations of specific operations
-    # for i in range(unique.shape[0]):
-    #     unique_operations_duration[unique[i]] = 0.0
-    #
-    # # print(unique_operations_duration)
-    #
-    # events_df = events_df.sort_values(by="Start")
-    # end = -1
-    #
-    # for index, row in events_df.iterrows():
-
-        # print(row["Op_Name"])
-        # print(row["Start"])
-        # print(row["Duration"])
-
-    #     func_end_time = row["Start"] + row["Duration"]
-    #
-    #     unique_operations_duration[row["Op_Name"]] += row["Duration"]
-    #     if func_end_time > end:
-    #         end = func_end_time
-    #
-    # print(end)
-    #
-    # console.print(res_table)
-    #
-    # print(unique_operations_duration)
-    # print('sum: ', sum(unique_operations_duration.values()))
-
-
 def print_total_vs_dls_time(total, dls_time_sum, variable_read_ops):
     console = Console()
     table = Table(show_header=True, header_style="bold magenta")
@@ -149,12 +95,12 @@ def print_csv_logfile_location(directory):
     console.print(table)
 
 
-def generate_otput_csv_file(events_df, dls_opts_df, path_to_logs):
+def generate_output_csv_file(events_df, dls_opts_df, path_to_logs):
     f = open(path_to_logs + '/output_data.csv', 'w')
     writer = csv.writer(f)
     writer.writerow(['name', 'time in milliseconds', 'ratio', 'occurrences'])
     # events_df.to_csv(path_to_logs + '/output_data_df.csv', index = False)
-    # TODO MARCEL add business logic
+    # TODO add business logic
     f.close()
 
 
@@ -185,10 +131,6 @@ def print_profiler_results(path_to_logs: str):
             shape = str("")
             is_DLS_Op = False
             occurrences = 0
-
-            # add key, value pair to dictionary
-            # if 'name' in json_data["traceEvents"][i] and json_data["traceEvents"][i].get("name") not in unique_operations.keys():
-            #     unique_operations[json_data["traceEvents"][i].get("name")] = 0.0
 
             if 'name' in json_data["traceEvents"][i]:
                 tmp_name = json_data["traceEvents"][i].get("name")
@@ -226,10 +168,7 @@ def print_profiler_results(path_to_logs: str):
         var_read_op = variable_read_op_time(events_df)
         print_total_vs_dls_time(total, dls_time_sum, var_read_op)
         print_profiler_results_dls_only(dls_opts_df, total)
-        # print_profiler_results_all(events_df, total)
-        generate_otput_csv_file(events_df, dls_opts_df, path_to_logs)
-
-        # print(unique_operations)
+        generate_output_csv_file(events_df, dls_opts_df, path_to_logs)
 
 
 profile_path = None
