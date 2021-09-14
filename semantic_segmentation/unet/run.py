8de0baf6086b9394a8ee86fa54b2cc9d83c52df5
MarcelWilnicki <marcel@onspecta.com>
2021-09-14 11:33:56 +0200

    add args

--- semantic_segmentation/unet/run.py
+++ semantic_segmentation/unet/run.py
@@ -1,9 +1,54 @@
 import os
-from utils.tf import TFFrozenModelRunner
+from utils.tf import TFSavedModelRunner
 
-img_width = 128
-img_height = 128
-img_channels = 3
 
-unet = TFFrozenModelRunner(path_to_model='/onspecta/dev/model_zoo/models/unet/unet.pb', output_names=['Identity'])
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run ResNet-50 v1.5 model.")
+    parser.add_argument("-m", "--model_path",
+                        type=str, required=True,
+                        help="path to the model")
+    parser.add_argument("-p", "--precision",
+                        type=str, choices=["fp32", "fp16", "int8"], required=True,
+                        help="precision of the model provided")
+    parser.add_argument("-b", "--batch_size",
+                        type=int, default=1,
+                        help="batch size to feed the model with")
+    parser.add_argument("--timeout",
+                        type=float, default=60.0,
+                        help="timeout in seconds")
+    parser.add_argument("--num_runs",
+                        type=int,
+                        help="number of passes through network to execute")
+    parser.add_argument("--images_path",
+                        type=str,
+                        help="path to directory with ImageNet validation images")
+    parser.add_argument("--labels_path",
+                        type=str,
+                        help="path to file with validation labels")
+    return parser.parse_args()
 
+
+def run_tf_fp32(model_path, batch_size, num_of_runs, timeout, images_path, labels_path):
+
+    def run_single_pass(tf_runner, imagenet):
+       pass
+
+    dataset = ImageNet(batch_size, "RGB", images_path, labels_path,
+                       pre_processing="Inception", is1001classes=False)
+    runner = TFSavedModelRunner(model_path, ["densenet169/predictions/Reshape_1:0"])
+
+    return run_model(run_single_pass, runner, dataset, batch_size, num_of_runs, timeout)
+
+
+def main():
+    args = parse_args()
+    if args.precision == "fp32":
+        run_tf_fp32(
+            args.model_path, args.batch_size, args.num_runs, args.timeout, args.images_path, args.labels_path
+        )
+    else:
+        assert False
+
+
+if __name__ == "__main__":
+    main()
