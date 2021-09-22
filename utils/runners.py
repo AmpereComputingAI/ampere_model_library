28fd3265c9e06196aa85ec8126c73492210bdd61
MarcelWilnicki <marcel@onspecta.com>
2021-09-22 17:15:11 +0200

    wip

--- utils/runners.py
+++ utils/runners.py
@@ -2,7 +2,9 @@ import utils.tf as utils_rn
 
 
 class UnetRunner(utils_rn.TFSavedModelRunner):
-
+    """
+    A class expanding on Saved Model Runner for unet-specific needs.
+    """
     def __init__(self, path_to_model: str):
         super().__init__(path_to_model)
         self.output_name = list(self.model.structured_outputs)[0]
