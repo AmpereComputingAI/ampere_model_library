ba581065544943c0b466d0af959546d059cef063
marcel.wilnicki <marcel.wilnicki@gmail.com>
2021-12-14 19:28:39 +0100

    wip

--- computer_vision/object_detection/ssd300_vgg_16/run.py
+++ computer_vision/object_detection/ssd300_vgg_16/run.py
@@ -46,57 +46,36 @@ def parse_args():
                         type=str,
                         choices=["pytorch"], required=True,
                         help="specify the framework in which a model should be run")
+    parser.add_argument("--jit_freeze", action='store_true',
+                        help="specify if model should be run with torch.jit.freeze model")
     return parser.parse_args()
 
 
-def run_pytorch_fp(batch_size, num_of_runs, timeout, images_path, anno_path, iou_tres, score_tres):
-    print(iou_tres, score_tres)
-    print(score_tres)
-
+def run_pytorch_fp(batch_size, num_of_runs, timeout, images_path, anno_path, jit_freeze):
     def run_single_pass(pytorch_runner, coco):
         shape = (300, 300)
         output = pytorch_runner.run(coco.get_input_array(shape))
+        if jit_freeze:
+            output = output[1]
 
-        # ========================================================================================
-        # jeden wariant \/ z ręcznym ustawianiem iou_tres, oraz score_tres
-        # doubled_boxes_removed = torchvision.ops.batched_nms(output[1][0]['boxes'], output[1][0]['scores'],
-        #                                                     output[1][0]['labels'], iou_tres)
-        #
-        # for i in range(batch_size):
-        #     for d in doubled_boxes_removed:
-        #         if output[1][i]['scores'][d.item()].item() > score_tres:
-        #             coco.submit_bbox_prediction(
-        #                 i,
-        #                 output[1][i]['boxes'][d.item()].tolist(),
-        #                 output[1][i]['scores'][d.item()].item(),
-        #                 output[1][i]['labels'][d.item()].item()
-        #             )
-
-        # print(output[1][0]['scores'][0].item())
-
-        # ========================================================================================
-        # drugi wariant
-        # print(output)
-        # quit()
         for i in range(batch_size):
-            for d in range(output[1][i]['boxes'].shape[0]):
+            for d in range(output[i]['boxes'].shape[0]):
                 coco.submit_bbox_prediction(
                     i,
-                    # output[1][i]['boxes'][d].tolist(),
-                    coco.convert_bbox_to_coco_order(output[1][i]['boxes'][d].tolist() * shape[0]),
-                    output[1][i]['scores'][d].item(),
-                    output[1][i]['labels'][d].item()
+                    coco.convert_bbox_to_coco_order(output[i]['boxes'][d].tolist()),
+                    output[i]['scores'][d].item(),
+                    output[i]['labels'][d].item()
                 )
 
     dataset = COCODataset(batch_size, "BGR", "COCO_val2014_000000000000", images_path, anno_path,
                           pre_processing="PyTorch_objdet", sort_ascending=True, order="CHW")
-    runner = PyTorchRunner(torchvision.models.detection.ssd300_vgg16(pretrained=True))
+    runner = PyTorchRunner(torchvision.models.detection.ssd300_vgg16(pretrained=True), jit_freeze=jit_freeze)
 
     return run_model(run_single_pass, runner, dataset, batch_size, num_of_runs, timeout)
 
 
-def run_pytorch_fp32(batch_size, num_of_runs, timeout, images_path, anno_path, iou_tres=None, score_tres=None):
-    return run_pytorch_fp(batch_size, num_of_runs, timeout, images_path, anno_path, iou_tres, score_tres)
+def run_pytorch_fp32(batch_size, num_of_runs, timeout, images_path, anno_path, jit_freeze):
+    return run_pytorch_fp(batch_size, num_of_runs, timeout, images_path, anno_path, jit_freeze)
 
 
 def run_tf_fp16(model_path, batch_size, num_of_runs, timeout, images_path, anno_path):
@@ -106,26 +85,10 @@ def run_tf_fp16(model_path, batch_size, num_of_runs, timeout, images_path, anno_
 def main():
     args = parse_args()
 
-    # ========================================================================================
-    # jeden wariant \/ z ręcznym ustawianiem iou_tres, oraz score_tres
-    iou_threshold = [0.0, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75,
-                     0.80, 0.85, 0.90, 0.95, 1.0]
-    score_threshold = [0.0, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70,
-                       0.75, 0.80, 0.85, 0.90, 0.95]
-
-    # for i in iou_threshold:
-    #     for s in score_threshold:
-    #             run_pytorch_fp32(args.batch_size, args.num_runs, args.timeout, IMAGES_PATH, ANNO_PATH, i, s)
-
-    # for s in score_threshold:
-    #     run_pytorch_fp32(args.batch_size, args.num_runs, args.timeout, IMAGES_PATH, ANNO_PATH, score_tres=s)
-
-    # ========================================================================================
-    # drugi wariant
     if args.framework == "pytorch":
         if args.precision == "fp32":
             run_pytorch_fp32(
-                args.batch_size, args.num_runs, args.timeout, IMAGES_PATH, ANNO_PATH
+                args.batch_size, args.num_runs, args.timeout, IMAGES_PATH, ANNO_PATH, args.jit_freeze
             )
         else:
             raise UnsupportedPrecisionValueError(args.precision)
