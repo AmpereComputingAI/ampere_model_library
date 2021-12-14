f25de2e79b37660bb99fccc4455c96af4242d3e5
marcel.wilnicki <marcel.wilnicki@gmail.com>
2021-12-14 18:05:42 +0100

    wip

--- utils/cv/coco.py
+++ utils/cv/coco.py
@@ -141,6 +141,7 @@ class COCODataset(ImageDataset):
         :param absolute: bool, if True right/bottom coordinates are being converted to shifts
         :return: bbox with reordered and converted data
         """
+
         left = bbox[left]
         top = bbox[top]
         right = bbox[right]
@@ -148,6 +149,7 @@ class COCODataset(ImageDataset):
         if absolute:
             right -= left
             bottom -= top
+
         return [left, top, right, bottom]
 
     def rescale_bbox(self, id_in_batch: int, bbox: list):
@@ -214,11 +216,17 @@ class COCODataset(ImageDataset):
         # img.save('/onspecta/Downloads/test.png')
         # quit()
 
+        # print('*' * 100)
+
         instance = list()
         instance.append(self.__current_image_ids[id_in_batch])
         instance += self.rescale_bbox(id_in_batch, bbox)
         instance.append(score)
         instance.append(category)
+
+        # print(instance)
+
+        # print('*' * 100)
         self.__detections.append(instance)
 
     def summarize_accuracy(self):
