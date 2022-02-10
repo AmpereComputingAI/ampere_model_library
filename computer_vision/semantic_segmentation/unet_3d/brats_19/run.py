eda235bf677e640c641322dee4e845194ec6a010
dkupnicki <97047305+dkupnicki@users.noreply.github.com>
2022-02-10 11:49:23 +0100

    add 3d-unet brats19 pytorch model (#92)
    * add 3d-unet brats19 pytorch model
    
    * make user point to model, not to folder
    
    * add BraTS 19 README
    
    * add more information about setting up the dataset and model

--- computer_vision/semantic_segmentation/unet_3d/brats_19/run.py
+++ computer_vision/semantic_segmentation/unet_3d/brats_19/run.py
@@ -1,12 +1,18 @@
 import argparse
+import os
 
 import numpy as np
+import pickle
+import torch
 
+import utils.cv.nnUNet.nnunet as nnunet
 from utils.cv.brats import BraTS19
 from utils.tf import TFFrozenModelRunner
+from utils.pytorch import PyTorchRunner
 from utils.benchmark import run_model
+from utils.cv.nnUNet.nnunet.training.model_restore import recursive_find_python_class
 
-from utils.misc import UnsupportedPrecisionValueError, FrameworkUnsupportedError
+from utils.misc import print_goodbye_message_and_die
 
 
 def parse_args():
@@ -28,7 +34,7 @@ def parse_args():
                         help="path to directory with BraTS19 dataset")
     parser.add_argument("--framework",
                         type=str,
-                        choices=["tf"], required=True,
+                        choices=["tf", "pytorch"], required=True,
                         help="specify the framework in which a model should be run")
     return parser.parse_args()
 
@@ -54,6 +60,37 @@ def run_tf_fp32(**kwargs):
 def run_tf_fp16(**kwargs):
     return run_tf_fp(**kwargs)
 
+def run_pytorch_fp32(model_path, num_runs, timeout, dataset_path, **kwargs):
+
+    def run_single_pass(pytorch_runner, brats):
+        output = pytorch_runner.run(torch.from_numpy(np.expand_dims(brats.get_input_array(), axis=0)))
+        output = np.asarray(output[0])
+        brats.submit_predictions(
+            output
+        )
+
+    dataset = BraTS19(dataset_dir_path=dataset_path)
+    model = restore_model(model_path)
+
+    runner = PyTorchRunner(model.network, disable_jit_freeze=True)
+    return run_model(run_single_pass, runner, dataset, 1, num_runs, timeout)
+
+def restore_model(checkpoint):
+    pkl_file = checkpoint + ".pkl"
+    with open(pkl_file, 'rb') as f:
+        info = pickle.load(f)
+    init = info['init']
+    name = info['name']
+    search_in = os.path.join(nnunet.__path__[0], "training", "network_training")
+    tr = recursive_find_python_class([search_in], name, current_module="nnunet.training.network_training")
+    trainer = tr(*init)
+    trainer.output_folder = f"{os.getcwd()}/result"
+    trainer.output_folder_base = "/result"
+    trainer.process_plans(info['plans'])
+    if checkpoint is not None:
+        trainer.load_checkpoint(checkpoint, False)
+    return trainer
+
 
 def main():
     args = parse_args()
@@ -69,6 +106,12 @@ def main():
         else:
             print_goodbye_message_and_die(
                 "this model seems to be unsupported in a specified precision: " + args.precision)
+    elif args.framework == "pytorch":
+        if args.precision == "fp32":
+            run_pytorch_fp32(**vars(args))
+        else:
+            print_goodbye_message_and_die(
+                "this model seems to be unsupported in a specified precision: " + args.precision)    
 
     else:
         print_goodbye_message_and_die(
