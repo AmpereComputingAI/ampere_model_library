7804daa5e5e6d90c6313c1b65313b126d81fc34d
Daniel Kupnicki <daniel@onspecta.com>
2022-04-12 14:04:21 +0200

    remove strict=False

--- utils/pytorch.py
+++ utils/pytorch.py
@@ -26,7 +26,7 @@ class PyTorchRunner:
             try:
                 self.__frozen_script = torch.jit.freeze(torch.jit.script(self.__model))
             except torch.jit.frontend.UnsupportedNodeError:
-                self.__frozen_script = torch.jit.freeze(torch.jit.trace(self.__model, example_inputs, strict=False))
+                self.__frozen_script = torch.jit.freeze(torch.jit.trace(self.__model, example_inputs=example_inputs))
 
         self.__warm_up_run_latency = 0.0
         self.__total_inference_time = 0.0
