88d7a198f25ce42584987da984f6de0afe067da0
Jan <jan@onspecta.com>
2022-04-14 14:57:32 +0000

    wip

--- setup_utils/install_frameworks.py
+++ setup_utils/install_frameworks.py
@@ -1,27 +1,43 @@
-from setuptools.command.easy_install import main as install
+import sys
+import subprocess
 
-# We skip installation if Ampere-optimized framework is present already
+native_frameworks = list()
+
+# We skip installation of given framework if an Ampere-optimized version is already present. If it is not, then we install the vanilla version (because we don't supply installation packages yet - go to https://solutions.amperecomputing.com/solutions/ampere-ai for optimized AI docker images from Ampere)
 
 # ONNXRunTime
 try:
     import onnxruntime as ort
-    print(ort.__version__)
     ort.AIO
 except (AttributeError, ModuleNotFoundError):
-    install(["onnxruntime==1.11.0"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "onnxruntime==1.11.0"])
+    native_frameworks.append("ONNXRunTime")
 
 # PyTorch
 try:
     import torch
-    print(torch.__version__)
     torch._C._aio_profiler_print()
 except (AttributeError, ModuleNotFoundError):
-    install(["torch==1.11.0"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "torch==1.11.0"])
+    native_frameworks.append("PyTorch")
 
 # TensorFlow
 try:
     import tensorflow as tf
-    print(tf.__version__)
     tf.AIO
 except (AttributeError, ModuleNotFoundError):
-    install(["tensorflow-aarch64==2.8.0"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "tensorflow-aarch64==2.8.0"])
+    native_frameworks.append("TensorFlow")
+
+len_native_frameworks = len(native_frameworks)
+if len_native_frameworks > 0:
+    native_frameworks_string = ""
+    if len_native_frameworks > 1:
+        for i in range(len_native_frameworks):
+            if i+2 == len_native_frameworks:
+                native_frameworks_string += f"{native_frameworks[i]} and "
+            elif i+1 == len_native_frameworks:
+                native_frameworks_string += f"{native_frameworks[i]}"
+            else:
+                native_frameworks_string += f"{native_frameworks[i]}, "
+    print(f"\033[1;31m \nCAUTION: {native_frameworks_string} frameworks have been installed in their native versions missing Ampere optimizations. Consider using AI-dedicated Docker images for increased performance. Available at: https://solutions.amperecomputing.com/solutions/ampere-ai")
