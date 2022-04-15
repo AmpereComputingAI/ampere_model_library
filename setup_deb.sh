f226e803bdf93146c6541525aaa083c8fd9cfcf1
Jan <jan@onspecta.com>
2022-04-15 14:45:57 +0000

    wip

--- setup_deb.sh
+++ setup_deb.sh
@@ -10,7 +10,13 @@ log() {
 
 log "Checking for Debian based Linux ..."
 sleep 1
-if ! [ -f "/etc/debian_version" ]; then
+if [ -f "/etc/debian_version" ]; then
+   debian_version=$(</etc/debian_version)
+   if [ "`echo "${debian_version} < 11.0" | bc`" -eq 1 ]; then
+       echo "Debian version >= 11.0 required! Quitting."
+       exit 1
+   fi
+else
    log "\nDebian-based Linux has not been detected! Quitting."
    exit 1
 fi
@@ -48,7 +54,7 @@ else
 fi
 pip3 install --no-deps --upgrade medpy==0.4.0 batchgenerators==0.21 medpy==0.4.0 nibabel==3.2.2 numpy==1.22.3 opencv-python==4.5.5.64 pandas==1.4.2 pycocotools==2.0.4 scikit-build==0.14.1 scipy==1.8.0 torchvision==0.10.0 transformers==4.18.0 tqdm==4.64.0
 pip3 install --no-deps --upgrade cycler==0.11.0 filelock==3.6.0 future==0.18.2 huggingface-hub==0.5.1 joblib==1.1.0 kiwisolver==1.4.2 matplotlib==3.5.1 nnunet==1.7.0 packaging==21.3 Pillow==9.1.0 pyparsing==3.0.8 python-dateutil==2.8.2 pytz==2022.1 pyyaml==6.0 regex==2022.3.15 sacremoses==0.0.49 scikit-image==0.19.2 scikit-learn==1.0.2 threadpoolctl==3.1.0 tokenizers==0.12.1
-python3 $SCRIPT_DIR/setup_utils/install_frameworks.py
+python3 $SCRIPT_DIR/utils/setup/install_frameworks.py
 log "done.\n"
 
 touch $SCRIPT_DIR/.setup_completed
