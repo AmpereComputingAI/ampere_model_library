fbb3ac11490a7f4c82dab7eaa0be5ed733ab1f2e
Jan <jan@onspecta.com>
2022-04-20 23:30:08 +0000

    wip

--- set_env_variables.sh
+++ set_env_variables.sh
@@ -26,7 +26,7 @@ log "done.\n"
 
 log "Setting environment variables ..."
 sleep 1
-LD_PRELOAD=$( find / -name "libgomp-*" )  # | grep "scikit_image.libs\|torch/lib" )
+LD_PRELOAD=$( find / -name "libgomp-*" | grep "\.so" )
 LD_PRELOAD="$( echo $LD_PRELOAD|awk -v OFS=":" '$1=$1' ):/lib/aarch64-linux-gnu/libGLdispatch.so.0"
 export LD_PRELOAD=$LD_PRELOAD
 echo "LD_PRELOAD=$LD_PRELOAD"
