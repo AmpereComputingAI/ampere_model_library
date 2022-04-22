47366941be61d9979cf075dd88a2062a8ab223d4
Jan Grzybek <52271793+jangrzybek@users.noreply.github.com>
2022-04-22 12:28:35 +0200

    Improve LD preload (#120)
    * restricts values in LD_PRELOAD to loadable ones

--- set_env_variables.sh
+++ set_env_variables.sh
@@ -6,15 +6,6 @@ log() {
   echo -e "${COLOR_CYAN}$1${COLOR_DEFAULT}"
 }
 
-log "Checking if script is sourced ..."
-sleep 1
-(return 0 2>/dev/null) && SOURCED=1 || SOURCED=0
-if [ $SOURCED != 1 ]; then
-   log "\nPlease source the script: 'source set_env_variables.sh'. Quitting."
-   exit 1
-fi
-log "done.\n"
-
 log "Checking if setup has been completed ..."
 sleep 1
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -24,13 +15,21 @@ if ! [ -f "$SCRIPT_DIR/.setup_completed" ]; then
 fi
 log "done.\n"
 
+log "Checking if script is sourced ..."
+sleep 1
+(return 0 2>/dev/null) && SOURCED=1 || SOURCED=0
+if [ $SOURCED != 1 ]; then
+   log "\nPlease source the script: 'source set_env_variables.sh'. Quitting."
+   exit 1
+fi
+log "done.\n"
+
 log "Setting environment variables ..."
 sleep 1
 ARCH=$( uname -m )
 if [ "${ARCH}" == "aarch64" ]; then
-   LD_PRELOAD=$( find / -name "libgomp*" | grep "\.so" )
-   # shellcheck disable=SC2086
-   LD_PRELOAD="$( echo $LD_PRELOAD|awk -v OFS=":" '$1=$1' ):/lib/aarch64-linux-gnu/libGLdispatch.so.0"
+   python3 $SCRIPT_DIR/utils/setup/gen_ld_preload.py
+   LD_PRELOAD=$(<$SCRIPT_DIR/utils/setup/.ld_preload)
    export LD_PRELOAD=$LD_PRELOAD
    echo "LD_PRELOAD=$LD_PRELOAD"
 fi
