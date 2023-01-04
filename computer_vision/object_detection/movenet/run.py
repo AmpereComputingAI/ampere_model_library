d0952020ecb029c9f477fea9f67cada8492f91aa
samhithaaaa <samhithamamindla037@gmail.com>
2023-01-03 19:59:23 -0800

    adding movenet and run.py first version

---
+++ computer_vision/object_detection/movenet/run.py
@@ -0,0 +1,86 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2022, Ampere Computing LLC
+
+# python run.py -m /Users/smamindla/Desktop/ml/Movenet/AML/ampere_model_library/computer_vision/object_detection/model.tflitef --images_path /Users/smamindla/Desktop/ml/Movenet/AML/ampere_model_library/computer_vision/object_detection/ --anno_path /Users/smamindla/Desktop/ml/Movenet/AML/ampere_model_library/computer_vision/object_detection/annotations/person_keypoints_val2017.json --framework tf
+
+import os
+import time
+import argparse
+import numpy as np
+import utils.misc as utils
+from utils.cv.movenet import MovenetDataset
+from utils.benchmark import run_model
+from utils.misc import print_goodbye_message_and_die
+from pycocotools.coco import COCO
+import tensorflow as tf
+
+KP_FILTERING_GT = 10
+BBOX_MIN_SIZE = 900
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run EfficientDet model.")
+    parser.add_argument("-m", "--model_path",
+                        type=str,
+                        help="path to the model")
+    parser.add_argument("-b", "--batch_size",
+                        type=int, default=1,
+                        help="batch size to feed the model with")
+    parser.add_argument("-f", "--framework",
+                        type=str,
+                        choices=["tf"], required=True,
+                        help="specify the framework in which a model should be run")
+    parser.add_argument("--timeout",
+                        type=float, default=60.0,
+                        help="timeout in seconds")
+    parser.add_argument("--num_runs",
+                        type=int,
+                        help="number of passes through network to execute")
+    parser.add_argument("--images_path",
+                        type=str,
+                        help="path to directory with COCO validation images")
+    parser.add_argument("--anno_path",
+                        type=str,
+                        help="path to file with validation annotations")
+    return parser.parse_args()
+
+
+def run_tflite(model_path, batch_size, num_runs, timeout, images_path, anno_path):
+    from utils.tflite import TFLiteRunner
+
+    def run_single_pass(tflite_runner, coco):
+        images, metadatas = next(coco.dataset)
+        input_image = tf.cast(images, dtype=tf.float32)
+
+        tflite_runner.set_input_tensor(tflite_runner.input_details[0]["index"], input_image)
+        tflite_runner.run()
+        keypoints_with_scores = tflite_runner.get_output_tensor(tflite_runner.output_details[0]["index"])
+        coco.submit_keypoint_prediction(images[0], keypoints_with_scores, metadatas[0])
+
+    runner = TFLiteRunner(model_path)
+    image_size = runner.input_details[0]['shape'][1]
+
+    dataset = MovenetDataset(anno_path, images_path, image_size)
+
+    
+
+    return run_model(run_single_pass, runner, dataset, batch_size, num_runs, timeout)
+
+
+def run_tflite_fp32(model_path, batch_size, num_runs, timeout, images_path, anno_path, **kwargs):
+    return run_tflite(model_path, 1, num_runs, timeout, images_path, anno_path)
+
+
+def main():
+    args = parse_args()
+    if args.framework == "tf":
+        if args.model_path is None:
+            print_goodbye_message_and_die(
+                "a path to model is unspecified!")
+        run_tflite_fp32(**vars(args))       
+    else:
+        print_goodbye_message_and_die(
+            "this model seems to be unsupported in a specified framework: " + args.framework)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/cv/movenet.py
@@ -0,0 +1,456 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+import skimage.io as io
+import tensorflow as tf
+from PIL import Image
+from tensorflow.keras.utils import Sequence
+from pycocotools.cocoeval import COCOeval
+from pycocotools.coco import COCO
+import numpy as np
+import pandas as pd
+import skimage.io as io
+from tqdm import tqdm
+from enum import Enum
+import numpy as np
+
+
+# Model Defaults
+DEFAULT_NUM_HG = 4 # NOTE in their final design, 8 were used
+DEFAULT_EPOCHS = 70
+DEFAULT_DATA_BASE_DIR = 'data'
+DEFAULT_MODEL_BASE_DIR = 'models'
+DEFAULT_LOGS_BASE_DIR = 'logs'
+DEFAULT_OUTPUT_BASE_DIR = 'output'
+
+DEFAULT_RESUME_DIR_FLAG = '_resume_'
+HPE_EPOCH_PREFIX = 'hpe_epoch'
+HPE_HOURGLASS_STACKS_PREFIX = 'hpe_hourglass_stacks'
+
+
+DEFAULT_LEARNING_RATE = 0.005
+
+# Order of keypoints in COCO dataset
+COCO_KEYPOINT_LABEL_ARR = ["nose", "left_eye", "right_eye", "left_ear", "right_ear", "left_shoulder", "right_shoulder", "left_elbow",
+                           "right_elbow", "left_wrist", "right_wrist", "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle"]
+
+# This array was copied from https://github.com/microsoft/human-pose-estimation.pytorch/blob/master/lib/dataset/coco.py
+# The original array was 1-indexed, so we subtract 1 from each element
+COCO_SKELETON = np.array([
+                    [16, 14],
+                    [14, 12],
+                    [17, 15],
+                    [15, 13],
+                    [12, 13],
+                    [6, 12],
+                    [7, 13],
+                    [6, 7],
+                    [6, 8],
+                    [7, 9],
+                    [8, 10],
+                    [9, 11],
+                    [2, 3],
+                    [1, 2],
+                    [1, 3],
+                    [2, 4],
+                    [3, 5],
+                    [4, 6],
+                    [5, 7]]) - 1
+
+
+# Model parameters
+INPUT_DIM = (256,256)
+INPUT_CHANNELS = 3
+OUTPUT_DIM = (64,64)
+NUM_CHANNELS = 256
+
+# Data Generator Constants
+DEFAULT_BATCH_SIZE = 12 #NOTE need to test optimal batch size
+NUM_COCO_KEYPOINTS = 17 # Number of joints to detect
+NUM_COCO_KP_ATTRBS = 3 # (x,y,v) * 17 keypoints
+BBOX_SLACK = 1.3 # before augmentation, increase bbox size to 130%
+
+KP_FILTERING_GT = 4 # Greater than x keypoints
+VAL_SHUFFLE = False
+
+# Data filtering constants
+BBOX_MIN_SIZE = 900 # Filter out images smaller than 30x30, TODO tweak
+
+# Output filtering constants
+HM_TO_KP_THRESHOLD = 0.2
+HM_TO_KP_THRESHOLD_POST_FILTER = HM_TO_KP_THRESHOLD / 5
+
+PCK_THRESHOLD = 0.2
+# This default PCK threshold is used when either hip is not present.
+# It is empirically chosen by taking the mean torso width in the validation set
+# {'mean': 25.273791558055517, 'std': 19.27466898776274}
+DEFAULT_PCK_THRESHOLD = PCK_THRESHOLD * 25
+
+# Output save names
+OUTPUT_STACKED_HEATMAP = 'heatmaps'
+OUTPUT_SKELETON = 'skeleton'
+OUTPUT_USER_IMG = 'user_img'
+
+
+#converting anns to df
+def get_meta(coco):
+    ids = list(coco.imgs.keys())
+    for i, img_id in enumerate(ids):
+        img_meta = coco.imgs[img_id]
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        anns = coco.loadAnns(ann_ids)
+        img_file_name = img_meta['file_name']
+        w = img_meta['width']
+        h = img_meta['height']
+        url = img_meta['coco_url']
+        
+        yield img_id, img_file_name, w, h, url, anns
+
+def convert_to_df(coco, data_set):
+    images_data = []
+    persons_data = []
+    gen = get_meta(coco)
+    for img_id, img_fname, w, h, url, meta in gen:        
+        images_data.append({
+            'image_id': int(img_id),
+            'src_set_image_id': int(img_id), # repeat id to reference after join
+            'coco_url': url,
+            'path': data_set + '/' + img_fname,
+            'width': int(w),
+            'height': int(h)
+        })
+        for m in meta:
+            persons_data.append({
+                'ann_id': m['id'],
+                'image_id': m['image_id'],
+                'is_crowd': m['iscrowd'],
+                'bbox': m['bbox'],
+                'bbox_area' : m['bbox'][2] * m['bbox'][3],
+                'area': m['area'],
+                'num_keypoints': m['num_keypoints'],
+                'keypoints': m['keypoints'],
+                'segmentation': m['segmentation']
+            })
+
+    images_df = pd.DataFrame(images_data)
+    images_df.set_index('image_id', inplace=True)
+
+    persons_df = pd.DataFrame(persons_data)
+    persons_df.set_index('image_id', inplace=True)
+
+    return images_df, persons_df
+
+def get_df(path_to_val_anns):    
+    val_coco = COCO(path_to_val_anns) # load annotations for validation set
+    images_df, persons_df = convert_to_df(val_coco, 'val2017')
+    val_coco_df = pd.merge(images_df, persons_df, right_index=True, left_index=True)
+
+    return val_coco_df
+    # ^ Dataframe containing all val and test keypoint annotations
+
+
+
+def transform_bbox_square(bbox, slack=1):
+    """
+        Transforms a bounding box anchored at top left corner of shape () to a square with
+        edge length being the larger of the bounding box's height or width.
+        Only supports square aspect ratios currently.
+        ## Parameters
+        bbox : {tuple or ndarray of len 4}
+            Given as two points, anchored at top left of image being 0,0: left, upper, right, lower
+        slack : {int, float}
+            The amount of extra padding that should be applied to the edges of the bounding box after
+            transforming
+        ##
+    """
+    x, y, w, h = [i for i in bbox]  # (x,y,w,h) anchored to top left
+    center_x = x+w/2
+    center_y = y+h/2
+
+    if w >= h:
+        new_w = w
+        new_h = w
+    else:
+        new_w = h
+        new_h = h
+
+    new_w *= slack  # add slack to bbox
+    new_h *= slack  # add slack to bbox
+    new_x = center_x - new_w/2
+    new_y = center_y - new_h/2
+    return (round(new_x), round(new_y), round(new_x+new_w), round(new_y+new_h))
+
+class MovenetDataset:
+
+    def __init__(self, anno_path, images_path, size):
+        df = get_df(anno_path)
+        df = df.loc[df['is_crowd'] == 0] # drop crowd anns
+        df = df.loc[df['num_keypoints'] > KP_FILTERING_GT] # drop anns containing x kps
+        df = df.loc[df['bbox_area'] > BBOX_MIN_SIZE]
+        df = df.reset_index(drop=True)
+
+        dataset = DataGenerator(df=df,base_dir=images_path,
+                                input_dim=(size,size),
+                                output_dim=(4,4),
+                                num_hg_blocks=1, # does not matter
+                                shuffle=True,
+                                batch_size=1,
+                                online_fetch=False,
+                                is_eval=True)
+        self.dataset = iter(dataset)
+        self.available_instances = len(dataset)
+        self.movenetcoco = MoveNetCoco()
+        self.cocoGt = COCO(anno_path)
+
+        self.image_ids = []
+        self.oks_values = [] 
+
+    def submit_keypoint_prediction(self, image_id, keypoints_with_scores, metadata):
+        pred = keypoints_with_scores[0,0].copy()
+        pred[:,:2] *= metadata['input_dim'][0]
+        pred[:,:2] = pred[:,:2].round()
+        pred[:,[0,1]] = pred[:,[1,0]]
+        image_id, oks = self.movenetcoco(metadata, pred.ravel() )
+        self.image_ids.append(image_id)
+        self.oks_values.append(oks)
+
+    def summarize_accuracy(self):
+        """
+        A function summarizing the accuracy achieved on the images obtained with get_input_array() calls on which
+        predictions done where supplied with submit_bbox_prediction() function.
+        """
+        print('***********',self.image_ids)
+        return oks_eval(self.image_ids, self.oks_values, self.cocoGt)
+
+class DataGenerator(Sequence):
+
+    def __init__(self, df, base_dir, input_dim, output_dim, num_hg_blocks, shuffle=False, \
+        batch_size=DEFAULT_BATCH_SIZE, online_fetch=False, is_eval=False):
+
+        self.df = df                    # df of the the annotations we want
+        self.base_dir = base_dir        # where to read imgs from in collab runtime        
+        self.input_dim = input_dim      # model requirement for input image dimensions
+        self.output_dim = output_dim    # dimesnions of output heatmap of model
+        self.num_hg_blocks = num_hg_blocks
+        self.shuffle = shuffle
+        self.batch_size = batch_size
+        self.is_eval = is_eval
+        # If true, images will be loaded from url over network rather than filesystem
+        self.online_fetch = online_fetch
+        self.on_epoch_end()
+
+    # after each epoch, shuffle indices so data order changes
+    def on_epoch_end(self):
+        self.indices = np.arange(len(self.df))
+        if self.shuffle:
+            np.random.shuffle(self.indices)
+
+    # number of batches (not number of examples)
+    def __len__(self):
+        return int(len(self.df) / self.batch_size)
+    #this transforms image 
+    def transform_image(self, img, bbox):
+        new_bbox = transform_bbox_square(bbox, slack=BBOX_SLACK)
+        cropped_img = img.crop(box=new_bbox)
+        cropped_width, cropped_height = cropped_img.size
+        new_img = cv2.resize(np.array(cropped_img), self.input_dim,
+                             interpolation=cv2.INTER_LINEAR)
+        return new_img, cropped_width, cropped_height, new_bbox[0], new_bbox[1]
+
+    #this transforms keypoints, both happen differently 
+    def transform_label(self, label, cropped_width, cropped_height, anchor_x, anchor_y):
+        label = [int(v) for v in label]
+        # adjust x/y coords to new resized img
+        transformed_label = []
+        for x, y, v in zip(*[iter(label)]*NUM_COCO_KP_ATTRBS):
+            x = round((x-anchor_x) * self.input_dim[0]/cropped_width)
+            y = round((y-anchor_y) * self.input_dim[1]/cropped_height)
+            # validate kps, throw away if out of bounds
+            # TODO: if kp is thrown away then we must update num_keypoints
+            if (x > self.input_dim[0] or x < 0) or (y > self.input_dim[1] or y < 0):
+                x, y, v = (0, 0, 0)
+
+            transformed_label.append(x)
+            transformed_label.append(y)
+            transformed_label.append(v)
+        return np.asarray(transformed_label) 
+    
+
+    # returns batch at index idx
+
+    """
+    Returns a batch from the dataset
+    ### Parameters:
+    idx : {int-type} Batch number to retrieve
+    ### Returns:    
+    X : ndarray of shape (batch number, input_dim1, input_dim2, 3)
+        This corresponds to a batch of images, normalized from [0,255] to [0,1]
+    """
+    def __getitem__(self, idx):
+        # Initialize Batch:
+        X = np.empty((self.batch_size, *self.input_dim, INPUT_CHANNELS))
+
+        metadatas = []
+        # get the indices of the requested batch
+        indices = self.indices[idx*self.batch_size:(idx+1)*self.batch_size]
+        for i, data_index in enumerate(indices):
+            ann = self.df.loc[data_index]
+            img_path = os.path.join(self.base_dir, ann['path'])
+
+            if self.online_fetch:
+                img = Image.fromarray(io.imread(ann['coco_url'])).convert('RGB')  # bottleneck opening from URL
+            else:
+                # bottleneck opening from file system
+                img = Image.open(img_path).convert('RGB')
+
+            transformed_img, cropped_width, cropped_height, anchor_x, anchor_y = self.transform_image(
+                img, ann['bbox'])
+            #transform label is transforming wrt keypoints
+            transformed_label = self.transform_label(
+                ann['keypoints'], cropped_width, cropped_height, anchor_x, anchor_y)
+
+            if self.is_eval:
+                metadata = {}
+                metadata["src_set_image_id"] = ann['src_set_image_id']
+                metadata["ann_id"] = ann['ann_id']
+                metadata["coco_url"] = ann['coco_url']
+                metadata["cropped_width"] = cropped_width
+                metadata["cropped_height"] = cropped_height
+                metadata["anchor_x"] = anchor_x
+                metadata["anchor_y"] = anchor_y
+                metadata["input_dim"] = self.input_dim
+                metadata["output_dim"] = self.output_dim
+                metadata["transformed_label"] = transformed_label #DEBUG
+                metadata["ground_truth_keypoints"] = ann['keypoints'] #DEBUG
+                metadatas.append(metadata)
+            #no need to scale
+            normalized_img = transformed_img
+            
+            X[i, ] = normalized_img  
+
+        if self.is_eval:
+            return X, metadatas
+
+        return X
+
+## taking pred of movenet and translate back keypoints(along with person) to original coco image dims
+class MoveNetCoco:
+
+    def __call__(self, metadata, untransformed_predictions):
+        metadata = self._undo_bounding_box_transformations(metadata, untransformed_predictions)
+        oks = self._create_oks_obj(metadata)
+        image_id = metadata['src_set_image_id']
+        return image_id, oks
+
+
+    def _undo_x(self, metadata, untransformed_x):
+      predicted_x = round(untransformed_x * metadata['cropped_width'] / metadata['input_dim'][0] + metadata['anchor_x'])
+      return round(predicted_x)
+
+    """
+        Parameters
+        ----------
+        metadata : object
+        should be metadata associated to a single image
+        untransformed_y : int
+        x coordinate to
+    """
+    def _undo_y(self, metadata, untransformed_y):
+      predicted_y = round(untransformed_y * metadata['cropped_height'] / metadata['input_dim'][1] + metadata['anchor_y'])
+      return round(predicted_y)
+
+    """
+        Parameters
+        ----------
+        metadata : object
+        should be metadata associated to a single image
+        untransformed_predictions : list
+        a list of precitions that need to be transformed
+        Example:  [1,2,0,1,4,666,32...]
+    """
+    def _undo_bounding_box_transformations(self, metadata, untransformed_predictions):
+        untransformed_predictions = untransformed_predictions.flatten()
+
+        predicted_labels = np.zeros(NUM_COCO_KEYPOINTS * NUM_COCO_KP_ATTRBS)
+        list_of_scores = np.zeros(NUM_COCO_KEYPOINTS)
+
+        for i in range(NUM_COCO_KEYPOINTS):
+            base = i * NUM_COCO_KP_ATTRBS
+
+            x = untransformed_predictions[base]
+            y = untransformed_predictions[base + 1]
+            conf = untransformed_predictions[base + 2]
+
+            if conf == 0:
+                # this keypoint is not predicted
+                x_new, y_new, vis_new = 0, 0, 0
+            else:
+                x_new = self._undo_x(metadata, x)
+                y_new = self._undo_y(metadata, y)
+                vis_new = 1
+                list_of_scores[i] = conf
+
+            predicted_labels[base]     = x_new
+            predicted_labels[base + 1] = y_new
+            predicted_labels[base + 2] = vis_new
+
+        metadata['predicted_labels'] = predicted_labels
+        metadata['score'] = float(np.mean(list_of_scores))
+        return metadata
+
+    def _create_oks_obj(self, metadata):
+        #oks is dict like iou, format that coco class is accepting to cal cross-val score
+        oks_obj = {}
+        oks_obj["image_id"] = int(metadata['src_set_image_id'])
+        oks_obj["ann_id"] = int(metadata['ann_id'])
+        oks_obj["category_id"] = 1
+        oks_obj["keypoints"] = metadata['predicted_labels']
+        oks_obj["score"] = float(metadata['score'])
+        return oks_obj
+
+
+
+
+def oks_eval(image_ids, list_of_predictions, cocoGt):
+    cocoDt=cocoGt.loadRes(list_of_predictions)
+
+    # Convert keypoint predictions to int type
+    for i in range(len(list_of_predictions)):
+        list_of_predictions[i]["keypoints"] = list_of_predictions[i]["keypoints"].astype('int')
+
+    annType = "keypoints"
+    cocoEval = COCOeval(cocoGt,cocoDt,annType)
+    cocoEval.params.imgIds = image_ids
+    cocoEval.params.catIds = [1] # Person category
+    cocoEval.evaluate()
+    cocoEval.accumulate()
+    print('\nSummary: ')
+    cocoEval.summarize()
+    stats = cocoEval.stats
+    oks = {
+        'Average Precision  (AP) @[ IoU=0.50:0.95 | area=   all | maxDets= 20 ]': stats[0],
+        'Average Precision  (AP) @[ IoU=0.50      | area=   all | maxDets= 20 ]': stats[1],
+        'Average Precision  (AP) @[ IoU=0.75      | area=   all | maxDets= 20 ]': stats[2],
+        'Average Precision  (AP) @[ IoU=0.50:0.95 | area=medium | maxDets= 20 ]': stats[3],
+        'Average Precision  (AP) @[ IoU=0.50:0.95 | area= large | maxDets= 20 ]': stats[4],
+        'Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets= 20 ]': stats[5],
+        'Average Recall     (AR) @[ IoU=0.50      | area=   all | maxDets= 20 ]': stats[6],
+        'Average Recall     (AR) @[ IoU=0.75      | area=   all | maxDets= 20 ]': stats[7],
+        'Average Recall     (AR) @[ IoU=0.50:0.95 | area=medium | maxDets= 20 ]': stats[8],
+        'Average Recall     (AR) @[ IoU=0.50:0.95 | area= large | maxDets= 20 ]': stats[9]
+    }
+    return oks
+
+
+
+KP_FILTERING_GT = 4
+BBOX_MIN_SIZE = 900
+if __name__ == '__main__':
+    path_to_val_anns = '/Users/smamindla/Desktop/ml/Movenet/AML/ampere_model_library/computer_vision/object_detection/annotations/person_keypoints_val2017.json'
+    df = get_df(path_to_val_anns)
+    df = df.loc[df['is_crowd'] == 0] # drop crowd anns
+    df = df.loc[df['num_keypoints'] > KP_FILTERING_GT] # drop anns containing x kps
+    df = df.loc[df['bbox_area'] > BBOX_MIN_SIZE]
+
+
