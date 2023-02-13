22d9af560bdd296f9769aad04a99e58fa33b7565
Daniel Kupnicki <dkupnicki@amperecomputing.com>
2023-02-13 13:06:02 +0000

    check if python*-dev available before install

--- setup_deb.sh
+++ setup_deb.sh
@@ -43,7 +43,10 @@ sleep 1
 apt-get update -y
 apt-get install -y python3 python3-pip build-essential ffmpeg libsm6 libxext6 wget
 PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))')
-apt-get -y install python${PYTHON_VERSION}-dev
+PYTHON_DEV_SEARCH=$(apt-cache search --names-only "python${PYTHON_VERSION}-dev")
+if [[ -n "$PYTHON_DEV_SEARCH"  ]]; then
+	apt-get -y install "python${PYTHON_VERSION}-dev"
+fi
 log "done.\n"
 
 log "Setup LD_PRELOAD ..."
