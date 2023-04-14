000c0d0a5bc096aa5208052187f5677ddc73187a
Daniel Kupnicki <dkupnicki@amperecomputing.com>
2023-04-14 16:47:31 +0000

    add ultralytics as dependency

--- setup_deb.sh
+++ setup_deb.sh
@@ -78,7 +78,8 @@ pip3 install --no-deps --upgrade \
    tqdm==4.64.0 \
    sacrebleu==2.3.1 \
    sentencepiece==0.1.97 \
-   tiktoken==0.3.3
+   tiktoken==0.3.3 \
+   ultralytics==8.0.75
 # dependencies of dependencies
 pip3 install --no-deps --upgrade \
    cycler==0.11.0 \
