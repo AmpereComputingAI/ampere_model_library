d4dc9d5e361bba559eb94ec296d41c5cdf1ab9d4
Michał Walenciak <mwalenciak@amperecomputing.com>
2023-05-08 13:34:27 +0000

    test another model

--- computer_vision/object_detection/retinanet/run.py
+++ computer_vision/object_detection/retinanet/run.py
@@ -61,7 +61,7 @@ def run_pytorch_fp(batch_size, num_runs, timeout, images_path, anno_path, disabl
 
     dataset = OpenImagesDataset(batch_size, "RGB", images_path, anno_path,
                                 pre_processing="YOLO", sort_ascending=True, order="NCHW")
-    runner = PyTorchRunner(torchvision.models.resnext50_32x4d(pretrained=True),
+    runner = PyTorchRunner(torchvision.models.detection.retinanet_resnet50_fpn(pretrained=True),
                            disable_jit_freeze=disable_jit_freeze)
 
     return run_model(run_single_pass, runner, dataset, batch_size, num_runs, timeout)
