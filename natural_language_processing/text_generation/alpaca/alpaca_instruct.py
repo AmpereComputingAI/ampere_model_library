c59ac351d9dadef5c7aec19a289847085a5ac088
Daniel Kupnicki <dkupnicki@amperecomputing.com>
2023-06-29 14:30:21 +0000

    add alpaca

---
+++ natural_language_processing/text_generation/alpaca/alpaca_instruct.py
@@ -0,0 +1,98 @@
+from collections import Counter
+import json
+
+class AlpacaInstruct:
+    """
+    A class providing facilities for preprocessing and postprocessing of Alpaca dataset.
+    """
+
+    def __init__(self, batch_size: int, tokenize_func=None, detokenize_func=None, dataset_path=None):
+        with open(dataset_path, "r") as dataset_file:
+            data = dataset_file.read()
+            self.data = json.loads(data)
+
+        self.available_instances = len(self.data)
+        self.__current_sample = 0
+        self.__exact_match_count = 0
+        self.__f1_count = 0
+
+    def get_input_array(self):
+        prompt = ("Below is an instruction that describes a task. Write a response that appropriately completes the request.\r\n\r\n"
+                  "### Instruction:\r\n"
+                 f"{self.data[self.__current_sample]['instruction']}\r\n\r\n")
+        if self.data[self.__current_sample]['input']:
+            prompt += ("### Input:\r\n"
+                      f"{self.data[self.__current_sample]['input']}\r\n\r\n")
+
+        prompt += "### Response:"
+
+        return prompt
+
+    def reset(self):
+        self.__current_sample = 0
+        return True
+    
+    def submit_prediction(self, answer: str):
+        """
+        A function allowing for a submission of obtained results of NLP inference.
+
+        :param answer: string, detokenized answer
+        """
+
+        def f1_score(normalized_prediction, normalized_ground_truth):
+            """
+            A function calculating the F1 score betweed normalized prediction and normalized ground truth.
+
+            :param normalized_prediction: str, normalized answer (prediction)
+            :param normalized_ground_truth: str, normalized correct answer (gt)
+            :return: float, f1 score
+            """
+            prediction_tokens = normalized_prediction.split()
+            ground_truth_tokens = normalized_ground_truth.split()
+            common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
+            num_same = sum(common.values())
+            if num_same == 0:
+                return 0
+            precision = 1.0 * num_same / len(prediction_tokens)
+            recall = 1.0 * num_same / len(ground_truth_tokens)
+            f1 = (2 * precision * recall) / (precision + recall)
+            return f1
+
+        def exact_match_score(normalized_prediction, normalized_ground_truth):
+            """
+            A function comparing normalized strings.
+
+            :param normalized_prediction: str, normalized answer (prediction)
+            :param normalized_ground_truth: str, normalized correct answer (gt)
+            :return: bool, True if strings equal
+            """
+            return normalized_prediction == normalized_ground_truth
+
+        def metric_max_over_ground_truth(metric_fn, prediction, ground_truth):
+            """
+            A function applying given metric function over provided correct answer (ground_truth).
+
+            :param metric_fn: function calculating a metric
+            :param prediction: str with predicted answer
+            :param ground_truth: string of correct answer
+
+            :return: float, max score obtained
+            """
+            scores_for_ground_truths = []
+            score = metric_fn(prediction, ground_truth)
+            scores_for_ground_truths.append(score)
+            return max(scores_for_ground_truths)
+
+        ground_truth = self.data[self.__current_sample]['output']
+        self.__exact_match_count += metric_max_over_ground_truth(exact_match_score, answer, ground_truth)
+        self.__f1_count += metric_max_over_ground_truth(f1_score, answer, ground_truth)
+        self.__current_sample += 1
+    
+    def summarize_accuracy(self):
+        exact_match = self.__exact_match_count / self.__current_sample
+        print("\n Exact match = {:.3f}".format(exact_match))
+        f1 = self.__f1_count / self.__current_sample
+        print(" F1 = {:.3f}".format(f1))
+
+        print(f"\nAccuracy figures above calculated on the basis of {self.__current_sample} instructions processed.")
+        return {"exact_match": exact_match, "f1": f1}
---
+++ natural_language_processing/text_generation/alpaca/run.py
@@ -0,0 +1,74 @@
+import argparse
+
+import transformers
+
+from alpaca_instruct import AlpacaInstruct
+from utils.pytorch import PyTorchRunner
+from utils.benchmark import run_model
+from utils.misc import print_goodbye_message_and_die
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run karpathy/nanoGPT with small modifications.")
+    parser.add_argument("-m", "--model_path",
+                        type=str, required=True,
+                        help="path to the model")
+    parser.add_argument("-b", "--batch_size",
+                        type=int, default=1,
+                        help="batch size to feed the model with")
+    parser.add_argument("-f", "--framework",
+                        type=str, default="pytorch",
+                        choices=["pytorch"],
+                        help="specify the framework in which a model should be run")
+    parser.add_argument("--timeout",
+                        type=float, default=60.0,
+                        help="timeout in seconds")
+    parser.add_argument("--num_runs",
+                        type=int,
+                        help="number of passes through network to execute")
+    parser.add_argument("--dataset_path",
+                        type=str,
+                        help="path to JSON file with instructions")
+    parser.add_argument("--disable_jit_freeze", action='store_true',
+                        help="if true model will be run not in jit freeze mode")
+    return parser.parse_args()
+
+
+def run_pytorch(model_path, batch_size, num_runs, timeout, dataset_path, disable_jit_freeze=False, **kwargs):
+
+    def run_single_pass(pytorch_runner, dataset):
+        inputs = encode(dataset.get_input_array())
+        outputs = pytorch_runner.run({"inputs": inputs.input_ids, "max_new_tokens": 100})
+        num_tokens = outputs.shape[1] - inputs.input_ids.shape[1]
+        response = decode(outputs[:, inputs.input_ids.shape[1]:])
+        dataset.submit_prediction(response)
+
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(model_path)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
+    dataset = AlpacaInstruct(batch_size, dataset_path=dataset_path)
+    encode = lambda i: tokenizer(i, return_tensors="pt")
+    decode = lambda t: tokenizer.batch_decode(t, skip_special_tokens=True)[0]
+
+    runner = PyTorchRunner(model, disable_jit_freeze=disable_jit_freeze, func="generate")
+
+    return run_model(run_single_pass, runner, dataset, batch_size, num_runs, timeout)
+
+
+def run_pytorch_fp32(model_path, batch_size, num_runs, timeout, dataset_path, disable_jit_freeze=False, **kwargs):
+    run_pytorch(model_path, batch_size, num_runs, timeout, dataset_path, disable_jit_freeze, **kwargs)
+
+
+def main():
+    args = parse_args()
+    if args.framework == "pytorch":
+        if args.batch_size != 1:
+            raise ValueError("Batch size must be 1 for this model.")
+        run_pytorch_fp32(**vars(args))
+    else:
+        print_goodbye_message_and_die(
+            "this model seems to be unsupported in a specified framework: " + args.framework)
+
+
+if __name__ == "__main__":
+    main()
