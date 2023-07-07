85819fd3b4f585402165ad1a66e27c2559218d42
Jan Grzybek <jan@amperecomputing.com>
2023-07-07 02:03:54 +0200

    wip

--- computer_vision/object_detection/yolo_v8/run.py
+++ computer_vision/object_detection/yolo_v8/run.py
@@ -78,7 +78,7 @@ def run_pytorch_fp(model_path, batch_size, num_runs, timeout, images_path, anno_
     def run_single_pass(pytorch_runner, coco):
         shape = (640, 640)
         inp = torch.stack(coco.get_input_array(shape))
-        output = pytorch_runner.run(inp)
+        output = pytorch_runner.run(batch_size, inp)
         output = ops.non_max_suppression(output)
 
         for i in range(batch_size):
