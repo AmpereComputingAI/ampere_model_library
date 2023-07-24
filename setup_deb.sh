e9112df90196c19c6f68a44f066da34684bfb7ca
dkupnicki <97047305+dkupnicki@users.noreply.github.com>
2023-07-24 12:59:39 +0200

    install Cython before the other packages

--- setup_deb.sh
+++ setup_deb.sh
@@ -62,6 +62,7 @@ log "done.\n"
 log "Installing python dependencies ..."
 sleep 1
 # direct dependencies
+pip3 install Cython==3.0.0
 pip3 install --no-deps --upgrade \
    SimpleITK==2.2.1 \
    batchgenerators==0.21 \
@@ -70,7 +71,6 @@ pip3 install --no-deps --upgrade \
    "numpy<1.24.0" \
    opencv-python==4.5.5.64 \
    pandas==1.4.2 \
-   Cython==3.0.0 \
    pycocotools==2.0.4 \
    scikit-build==0.14.1 \
    scipy==1.8.0 \
