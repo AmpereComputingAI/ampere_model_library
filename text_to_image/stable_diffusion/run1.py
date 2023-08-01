f1850c1bd8fa618abf009cbdd73b9e488600394a
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-08-01 14:52:44 +0200

    wip

--- text_to_image/stable_diffusion/run1.py
+++ text_to_image/stable_diffusion/run1.py
@@ -26,6 +26,18 @@ def run_pytorch_fp32(args):
     model = load_model_from_config(config, f"{args.ckpt}", device)
     sampler = DDIMSampler(model, device=device)
 
+    C = args.C
+    H = args.H
+    f = args.f
+    W = args.w
+
+    n_samples = args.n_samples
+    steps = args.steps
+    scale = args.scale
+    ddim_eta = args.ddim_eta
+    fixed_code = args.fixed_code
+    batch_size = args.batch_size
+
     def single_pass_pytorch(_runner, _stablediffusion):
         # array = _stablediffusion.get_input()
         _stablediffusion.submit_count(
@@ -35,26 +47,26 @@ def run_pytorch_fp32(args):
     def wrapper():
         c = model.get_learned_conditioning(["a professional photograph of an astronaut riding a triceratops"])
         uc = None
-        if args.scale != 1.0:
-            uc = model.get_learned_conditioning(args.batch_size * [""])
-        shape = [args.C, args.H // args.f, args.W // args.f]
+        if scale != 1.0:
+            uc = model.get_learned_conditioning(batch_size * [""])
+        shape = [C, H // f, W // f]
 
         start_code = None
-        if args.fixed_code:
-            start_code = torch.randn([args.n_samples, args.C, args.H // args.f, args.W // args.f], device=device)
+        if fixed_code:
+            start_code = torch.randn([n_samples, C, H // f, W // f], device=device)
 
-        samples, _ = sampler.sample(S=args.steps,
+        samples, _ = sampler.sample(S=steps,
                                     conditioning=c,
-                                    batch_size=args.n_samples,
+                                    batch_size=n_samples,
                                     shape=shape,
                                     verbose=False,
-                                    unconditional_guidance_scale=args.scale,
+                                    unconditional_guidance_scale=scale,
                                     unconditional_conditioning=uc,
-                                    eta=args.ddim_eta,
+                                    eta=ddim_eta,
                                     x_T=start_code)
 
-    def sampler_wrapper(args):
-        return main(args)
+    # def sampler_wrapper(args):
+    #     return main(args)
 
     runner = PyTorchRunnerV2(wrapper)
     stablediffusion = StableDiffusion()
@@ -67,7 +79,6 @@ if __name__ == "__main__":
     parser = DefaultArgParser(["pytorch"])
     parser.ask_for_batch_size()
     parser.add_argument("--steps", type=int, default=25, help="steps through which the model processes the input")
-    parser.add_argument("-w", '--width', type=int, default=512, help="width of the image")
     parser.add_argument('--scale', type=int, default=9, help="scale of the image")
     parser.add_argument("--seed", type=int, default=42, help="the seed (for reproducible sampling)",)
     parser.add_argument("--config", type=str,
@@ -83,7 +94,7 @@ if __name__ == "__main__":
     parser.add_argument("--H", type=int, default=512, help="image height, in pixel space")
     parser.add_argument("--W", type=int, default=512, help="image width, in pixel space")
     parser.add_argument("--C", type=int, default=4, help="latent channels")
-    parser.add_argument("--f", type=int, default=8, help="downsampling factor, most often 8 or 16")
+    parser.add_argument("--f", type=int, default=8, help="down-sampling factor, most often 8 or 16")
     parser.add_argument("--device", type=str, help="Device on which Stable Diffusion will be run",
                         choices=["cpu", "cuda"], default="cpu")
     parser.add_argument("--n_iter", type=int, default=1, help="sample this often")
