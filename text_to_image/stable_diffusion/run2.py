37663e82cb0a39394af59b0e5087c4a3a671078a
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-08-09 15:15:32 +0200

    wip

--- text_to_image/stable_diffusion/run2.py
+++ text_to_image/stable_diffusion/run2.py
@@ -63,7 +63,7 @@ def run_pytorch_fp32(args):
     torchscript = args.torchscript
     ipex = args.ipex
     bf16 = args.bf16
-    precision = args.precision
+    precision = "full"
     n_iter = args.n_iter
 
     seed_everything(seed)
@@ -245,13 +245,9 @@ if __name__ == "__main__":
     parser.add_argument("--fixed_code", action='store_true', help="if enabled, uses the same starting code across all samples ")
 
     parser.add_argument("--ipex", action='store_true', help="Use Intel® Extension for PyTorch*")
-    parser.add_argument("--precision", type=str, help="evaluate at this precision", choices=["full", "autocast"], default="autocast")
     parser.add_argument("--bf16", action='store_true', help="use bfloat16")
     parser.add_argument("--n_iter", type=int, default=3, help="sample this often")
-    parser.add_argument("--plms", action='store_true', help="use plms sampling")
-    parser.add_argument("--dpm", action='store_true', help="use DPM (2) sampler")
-    parser.add_argument("--from-file", type=str, help="if specified, load prompts from this file, separated by newlines")
     parser.add_argument("--repeat", type=int, default=1, help="repeat each prompt in file this often")
-    parser.add_argument("--torchscript", action='store_true', help="Use TorchScript")
+
 
     run_pytorch_fp32(parser.parse())
