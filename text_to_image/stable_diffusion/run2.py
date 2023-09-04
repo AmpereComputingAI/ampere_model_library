9ccb784bfc0b8a7ce6576609e393cd6e2bb88673
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-09-04 14:10:50 +0200

    wip

--- text_to_image/stable_diffusion/run2.py
+++ text_to_image/stable_diffusion/run2.py
@@ -12,12 +12,12 @@ from torchvision.utils import make_grid
 from utils.downloads.utils import get_downloads_path
 
 
-# def put_watermark(img, wm_encoder=None):
-#     if wm_encoder is not None:
-#         img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-#         img = wm_encoder.encode(img, 'dwtDct')
-#         img = Image.fromarray(img[:, :, ::-1])
-#     return img
+def put_watermark(img, wm_encoder=None):
+    if wm_encoder is not None:
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        img = wm_encoder.encode(img, 'dwtDct')
+        img = Image.fromarray(img[:, :, ::-1])
+    return img
 
 
 def run_pytorch_fp32(model_path, config, steps, scale, prompt, outdir, batch_size, num_runs, timeout):
@@ -41,14 +41,14 @@ def run_pytorch_fp32(model_path, config, steps, scale, prompt, outdir, batch_siz
 
     # =========================
     # TODO: stuff for saving images, used to evaluate output, to be removed when accuracy measures are implemented
-    # os.makedirs(outdir, exist_ok=True)
-    # outpath = outdir
-    # sample_path = os.path.join(outpath, "samples")
-    # os.makedirs(sample_path, exist_ok=True)
-    # print("Creating invisible watermark encoder (see https://github.com/ShieldMnt/invisible-watermark)...")
-    # wm = "SDV2"
-    # wm_encoder = WatermarkEncoder()
-    # wm_encoder.set_watermark('bytes', wm.encode('utf-8'))
+    os.makedirs(outdir, exist_ok=True)
+    outpath = outdir
+    sample_path = os.path.join(outpath, "samples")
+    os.makedirs(sample_path, exist_ok=True)
+    print("Creating invisible watermark encoder (see https://github.com/ShieldMnt/invisible-watermark)...")
+    wm = "SDV2"
+    wm_encoder = WatermarkEncoder()
+    wm_encoder.set_watermark('bytes', wm.encode('utf-8'))
 
     # =========================
     # TODO: torchscript stuff, should it stay here?
@@ -87,10 +87,10 @@ def run_pytorch_fp32(model_path, config, steps, scale, prompt, outdir, batch_siz
         _stablediffusion.submit_count()
 
     def wrapper():
-        # all_samples = list()
-        # base_count = len(os.listdir(sample_path))
-        # sample_count = 0
-        # grid_count = len(os.listdir(outpath)) - 1
+        all_samples = list()
+        base_count = len(os.listdir(sample_path))
+        sample_count = 0
+        grid_count = len(os.listdir(outpath)) - 1
 
         with torch.no_grad(), nullcontext(torch.device("cpu")), model.ema_scope():
             uc = model.get_learned_conditioning(batch_size * [""]) if scale != 1.0 else None
@@ -106,33 +106,33 @@ def run_pytorch_fp32(model_path, config, steps, scale, prompt, outdir, batch_siz
                                         eta=0.0,
                                         x_T=None)
 
-        # x_samples = model.decode_first_stage(samples)
-        # x_samples = torch.clamp((x_samples + 1.0) / 2.0, min=0.0, max=1.0)
-        #
-        # for x_sample in x_samples:
-        #     x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-        #     img = Image.fromarray(x_sample.astype(np.uint8))
-        #     img = put_watermark(img, wm_encoder)
-        #     img.save(os.path.join(sample_path, f"{base_count:05}.png"))
-        #     base_count += 1
-        #     sample_count += 1
-        #
-        # all_samples.append(x_samples)
-        #
-        # # additionally, save as grid
-        #
-        # grid = torch.stack(all_samples, 0)
-        # grid = rearrange(grid, 'n b c h w -> (n b) c h w')
-        # grid = make_grid(grid, nrow=1)
-        #
-        # # to image
-        # grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-        # grid = Image.fromarray(grid.astype(np.uint8))
-        # grid = put_watermark(grid, wm_encoder)
-        # grid.save(os.path.join(outpath, f'grid-{grid_count:04}.png'))
-        # grid_count += 1
-        #
-        # print(f"Your samples are ready and waiting for you here: \n{outpath}")
+        x_samples = model.decode_first_stage(samples)
+        x_samples = torch.clamp((x_samples + 1.0) / 2.0, min=0.0, max=1.0)
+
+        for x_sample in x_samples:
+            x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+            img = Image.fromarray(x_sample.astype(np.uint8))
+            img = put_watermark(img, wm_encoder)
+            img.save(os.path.join(sample_path, f"{base_count:05}.png"))
+            base_count += 1
+            sample_count += 1
+
+        all_samples.append(x_samples)
+
+        # additionally, save as grid
+
+        grid = torch.stack(all_samples, 0)
+        grid = rearrange(grid, 'n b c h w -> (n b) c h w')
+        grid = make_grid(grid, nrow=1)
+
+        # to image
+        grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
+        grid = Image.fromarray(grid.astype(np.uint8))
+        grid = put_watermark(grid, wm_encoder)
+        grid.save(os.path.join(outpath, f'grid-{grid_count:04}.png'))
+        grid_count += 1
+
+        print(f"Your samples are ready and waiting for you here: \n{outpath}")
 
     runner = PyTorchRunnerV2(wrapper)
     stablediffusion = StableDiffusion()
