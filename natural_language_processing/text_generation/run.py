9c68a7d1bed542a9dc1c9769db7157bbb0d44096
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-09-08 14:10:10 +0200

    wip

--- natural_language_processing/text_generation/run.py
+++ natural_language_processing/text_generation/run.py
@@ -1,11 +1,25 @@
+import torch
+import time
 from transformers import GPT2Tokenizer, GPT2Model
 
-model = GPT2Model.from_pretrained('gpt2')
+model = GPT2Model.from_pretrained('gpt2', torchscript=True)
+model.eval()
+model = torch.jit.freeze(torch.jit.script(model))
+
+
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
 text = "Replace me by any text you'd like."
 encoded_input = tokenizer(text, return_tensors='pt')
 
+with torch.no_grad():
+    start = time.time()
+    output = model(**encoded_input)
+    finish = time.time()
 
-output = model(**encoded_input)
+
+
+    print(finish - start)
+
+
 
