d9b38cee1cc24795e660a82653eb75d077d7f7e7
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-09-08 14:50:48 +0200

    wip

--- natural_language_processing/text_generation/run.py
+++ natural_language_processing/text_generation/run.py
@@ -3,20 +3,13 @@ import time
 from transformers import GPT2Tokenizer, GPT2Model
 
 model = GPT2Model.from_pretrained('gpt2', torchscript=True)
-model.eval()
-model = torch.jit.freeze(torch.jit.script(model))
-
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-
-text = "Replace me by any text you'd like."
-encoded_input = tokenizer(text, return_tensors='pt')
-
-traced_model = torch.jit.trace(model, (encoded_input,))
-frozen_model = torch.jit.freeze(traced_model)
+example_input = tokenizer.encode("Once upon a time", return_tensors="pt")
+model = torch.jit.freeze(torch.jit.trace(model, (example_input,)))
 
 with torch.no_grad():
     start = time.time()
-    output = model.generate(encoded_input, max_length=50, num_return_sequences=1)
+    output = model.generate(example_input, max_length=50, num_return_sequences=1)
     finish = time.time()
     print(finish - start)
