3b0a27d6655765c41246e371e6695860e76a030d
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-09-08 14:55:48 +0200

    wip

--- natural_language_processing/text_generation/run.py
+++ natural_language_processing/text_generation/run.py
@@ -2,9 +2,10 @@ import torch
 import time
 from transformers import GPT2Tokenizer, GPT2Model
 
-model = GPT2Model.from_pretrained('gpt2', torchscript=True)
-
+model = GPT2Model.from_pretrained('gpt2')
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+
 example_input = tokenizer.encode("Once upon a time", return_tensors="pt")
 model = torch.jit.freeze(torch.jit.trace(model, (example_input,)))
 
