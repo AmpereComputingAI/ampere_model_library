7524006fedc661f65f338775c3d1ec9316846a37
Daniel Kupnicki <dkupnicki@amperecomputing.com>
2023-09-22 16:06:58 +0000

    aio works

--- natural_language_processing/text_generation/alpaca/run.py
+++ natural_language_processing/text_generation/alpaca/run.py
@@ -12,7 +12,7 @@ def run_pytorch(model_path, num_runs, timeout, dataset_path, disable_jit_freeze=
 
     def run_single_pass(pytorch_runner, dataset):
         inputs = encode(dataset.get_input_array())
-        outputs = pytorch_runner.run(inputs=inputs.input_ids, max_new_tokens=100)
+        outputs = pytorch_runner.run(inputs=inputs.input_ids, max_new_tokens=10)
         pytorch_runner.set_task_size(outputs.shape[1] - inputs.input_ids.shape[1])
         response = decode(outputs[:, inputs.input_ids.shape[1]:])
         dataset.submit_prediction(response)
@@ -20,8 +20,8 @@ def run_pytorch(model_path, num_runs, timeout, dataset_path, disable_jit_freeze=
 
     model = transformers.AutoModelForCausalLM.from_pretrained(model_path)
     model.eval()
-    aio = '_aio_profiler_print' in dir(torch._C)
-    model = apply_compile_maybe(model, aio)
+    aio = '_aio_profiler_print' in dir(torch._C) and os.environ.get("AIO_PROCESS_MODE") != "0"
+    model.greedy_search = apply_compile_maybe(model.greedy_search, aio)
 
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
     dataset = AlpacaInstruct(1, dataset_path=dataset_path)
