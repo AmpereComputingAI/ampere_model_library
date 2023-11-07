5dfaac524788da0dc89f60f58ab2271618367e3f
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-11-07 16:03:31 +0100

    wip

---
+++ natural_language_processing/text_generation/gpt-j/run1.py
@@ -0,0 +1,19 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+# model = GPT2Model.from_pretrained(model_name, torchscript=True)
+
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", pad_token_id=tokenizer.eos_token_id, torchscript=True)
+text = "Hi, how are you?"
+encoded_input = tokenizer(text, return_tensors='pt')
+input_dict = {key: value for key, value in encoded_input.items()}
+
+
+traced_model = torch.jit.trace(model, (input_dict['input_ids'],))
+#traced_model = torch.jit.trace(model, (encoded_input,))
+frozen_model = torch.jit.freeze(traced_model)
+
+#output = frozen_model(**encoded_input)
+output = frozen_model(input_dict['input_ids'])
