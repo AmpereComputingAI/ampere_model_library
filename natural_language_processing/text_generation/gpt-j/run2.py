d4da8db9034c375617c94370cc3c91fde89d1452
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-11-08 11:21:34 +0100

    wip

--- natural_language_processing/text_generation/gpt-j/run2.py
+++ natural_language_processing/text_generation/gpt-j/run2.py
@@ -8,6 +8,8 @@ tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", pad_token_id=tokenizer.eos_token_id)
 text = "Hi, how are you?"
 encoded_input = tokenizer.encode(text, return_tensors='pt')
+model.eval()
+torch.jit.script(model)
 output = model.generate(encoded_input, max_length=30, num_beams=2, no_repeat_ngram_size=2, early_stopping=True)
 
 print(output)
