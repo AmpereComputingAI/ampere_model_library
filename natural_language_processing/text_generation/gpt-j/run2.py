391b7055a1b27f0df2452ed1d8304cafa1657e00
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-11-08 11:50:02 +0100

    wip

--- natural_language_processing/text_generation/gpt-j/run2.py
+++ natural_language_processing/text_generation/gpt-j/run2.py
@@ -5,14 +5,21 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 # model = GPT2Model.from_pretrained(model_name, torchscript=True)
 
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", pad_token_id=tokenizer.eos_token_id, torchscript=True)
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", torchscript=True).eval()
 text = "Hi, how are you?"
 encoded_input = tokenizer.encode(text, return_tensors='pt')
-model.eval()
-torch.jit.script(model)
-output = model.generate(encoded_input, max_length=30, num_beams=2, no_repeat_ngram_size=2, early_stopping=True)
 
-print(output)
-print(type(output))
+model = torch.jit.trace(model, torch.randint(10000, (5,)))
+scripted_model = torch.jit.script(model)
+# output = model.generate(encoded_input, max_length=30, num_beams=2, no_repeat_ngram_size=2, early_stopping=True)
+output = scripted_model(encoded_input)
+# torch_out = scripted_model(context)
+generated_text_torch = tokenizer.decode(output)
 
-print(tokenizer.decode(output[0], skip_special_tokens=True))
+# print("Fragment: {}".format(sentence_fragment))
+print("Completed: {}".format(generated_text_torch))
+
+# print(output)
+# print(type(output))
+#
+# print(tokenizer.decode(output[0], skip_special_tokens=True))
