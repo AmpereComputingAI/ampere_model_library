5ecb46459b69f1ffcbaa22d6c9c355aea3557de0
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-11-08 12:22:09 +0100

    wip

--- natural_language_processing/text_generation/gpt-j/run-v2.py
+++ natural_language_processing/text_generation/gpt-j/run-v2.py
@@ -32,10 +32,10 @@ def run_pytorch_fp32(model_name, batch_size, num_runs, timeout, lambada_path, **
     model.eval()
     # model = apply_jit_trace(model, (dataset.get_input_array()[0],))
     model = apply_jit_trace(model, torch.randint(10000, (5,)))
-    model = apply_jit_script(model)
+    with torch.no_grad:
+        model = apply_jit_script(model)
 
     runner = PyTorchRunnerV2(model)
-
 
     # runner = PyTorchRunner(model, disable_jit_freeze=False, func="generate")
     # runner = PyTorchRunnerV2(model)
