04097619b9957db56efbec7fd2865f7ba828b3f0
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-11-27 17:03:13 +0100

    wip

--- natural_language_processing/text_generation/gpt-j/run2.py
+++ natural_language_processing/text_generation/gpt-j/run2.py
@@ -5,6 +5,7 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 model = GPT2LMHeadModel.from_pretrained("gpt2")
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
+model.eval()
 # Prepare the input
 sentence = "Hello, how are you?"
 inputs = tokenizer.encode(sentence, return_tensors="pt")
