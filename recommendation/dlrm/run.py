130ef755611124bf8e0df7298029e616ebb9f68a
Daniel Kupnicki <dkupnicki@amperecomputing.com>
2023-12-08 13:21:40 +0000

    dlrm works on gpu

--- recommendation/dlrm/run.py
+++ recommendation/dlrm/run.py
@@ -81,6 +81,45 @@ def run_pytorch_fp(model_path, batch_size, num_runs, timeout, dataset_path, debu
     return run_model(run_single_pass, runner, dataset, batch_size, num_runs, timeout)
 
 
+def run_pytorch_cuda(model_path, batch_size, num_runs, timeout, dataset_path, debug):
+    from utils.pytorch import PyTorchRunnerV2
+
+    def run_single_pass(torch_runner, criteo):
+        output = torch_runner.run(batch_size, *tuple(i.cuda() for i in criteo.get_inputs())).cpu()
+        criteo.submit_predictions(output)
+
+    append_dlrm_to_pypath()
+    from utils.recommendation.dlrm.dlrm_s_pytorch import DLRM_Net
+
+    dataset = Criteo(max_batch_size=batch_size, dataset_path=dataset_path, debug=debug)
+
+    if not debug:
+        m_spa = 128
+        ln_bot = np.array([13, 512, 256, 128])
+        ln_top = np.array([479, 1024, 1024, 512, 256, 1])
+    else:
+        m_spa = 64
+        ln_bot = np.array([13, 512, 256, 64])
+        ln_top = np.array([415, 512, 512, 256, 1])
+    dlrm = DLRM_Net(
+        m_spa=m_spa,
+        ln_emb=dataset.ln_emb,
+        ln_bot=ln_bot,
+        ln_top=ln_top,
+        arch_interaction_op="dot",
+        sigmoid_top=ln_top.size-2,
+        qr_operation="mult",
+        qr_collisions=4,
+        qr_threshold=200,
+        md_threshold=200,
+    )
+    dlrm.load_state_dict(torch.load(model_path)["state_dict"])
+
+    runner = PyTorchRunnerV2(dlrm.cuda())
+
+    return run_model(run_single_pass, runner, dataset, batch_size, num_runs, timeout)
+
+
 def run_pytorch_fp32(model_path, batch_size, num_runs, timeout, dataset_path, debug):
     return run_pytorch_fp(model_path, batch_size, num_runs, timeout, dataset_path, debug)
 
