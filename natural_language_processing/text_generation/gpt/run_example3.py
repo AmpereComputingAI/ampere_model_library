3f4cf944929c0e9ae9aa3b3fbde377de60de2381
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-12-12 16:55:55 +0100

    wip

--- natural_language_processing/text_generation/gpt/run_example3.py
+++ natural_language_processing/text_generation/gpt/run_example3.py
@@ -25,10 +25,18 @@ with torch.no_grad():
 model.generate = torch.jit.freeze(torch.jit.trace_module(model, {"generate": inputs}))
 
 print("\nTracing engaged\n")
+# with torch.no_grad():
+#     for n in range(3):
+#         x = time.time()
+#         outputs = model.generate(inputs, do_sample=True, max_length=100, top_k=50, top_p=0.95, num_return_sequences=1)
+#         print(f"Run: {n}, throughput: {round(outputs.shape[1] / (time.time() - x), 3)} tps")
+# text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+# print(text)
+
+
 with torch.no_grad():
-    for n in range(3):
-        x = time.time()
-        outputs = model.generate(inputs, do_sample=True, max_length=100, top_k=50, top_p=0.95, num_return_sequences=1)
-        print(f"Run: {n}, throughput: {round(outputs.shape[1] / (time.time() - x), 3)} tps")
+    x = time.time()
+    outputs = model.generate(inputs, do_sample=True, max_length=20, top_p=0.95)
+    print(f"throughput: {round(outputs.shape[1] / (time.time() - x), 3)} tps")
 text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 print(text)
