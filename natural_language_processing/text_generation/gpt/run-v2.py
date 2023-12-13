006bacb4d1461991494a39fae25afa14c3f8c48d
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-12-13 15:05:04 +0100

    wip

--- natural_language_processing/text_generation/gpt/run-v2.py
+++ natural_language_processing/text_generation/gpt/run-v2.py
@@ -1,27 +1,30 @@
 import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, AutoTokenizer
 
 from utils.benchmark import run_model
 from utils.nlp.lambada import Lambada
 
 
 def run_pytorch_fp32(model_name, batch_size, num_runs, timeout, lambada_path, **kwargs):
-    from utils.pytorch import PyTorchRunner, PyTorchRunnerV2, apply_jit_trace, apply_jit_script
+    from utils.pytorch import PyTorchRunner, PyTorchRunnerV2, apply_jit_trace, apply_jit_script, apply_jit_trace_module
 
     def run_single_pass(pytorch_runner, lambada):
         start_ids = lambada.get_input_array()[0]
         # output = pytorch_runner.run(None, start_ids, num_beams=2, no_repeat_ngram_size=2,
         #                             early_stopping=True, max_new_tokens=5)
-        output = pytorch_runner.run(None, start_ids)
-        pytorch_runner.set_task_size(output[1] - start_ids.shape[1])
-        logits = output[0]
-        token_ids = torch.argmax(logits, dim=-1)
+        output = pytorch_runner.run(None, start_ids, do_sample=True, max_length=50, top_p=0.95)
 
-        print(type(token_ids))
-        print(token_ids)
-        text = tokenizer.decode(token_ids)
-        print(text)
         quit()
+        # output = pytorch_runner.run(None, start_ids)
+        # pytorch_runner.set_task_size(output[1] - start_ids.shape[1])
+        # logits = output[0]
+        # token_ids = torch.argmax(logits, dim=-1)
+        #
+        # print(type(token_ids))
+        # print(token_ids)
+        # text = tokenizer.decode(token_ids)
+        # print(text)
+        # quit()
 
         output = detokenize(output[0])
 
@@ -29,7 +32,8 @@ def run_pytorch_fp32(model_name, batch_size, num_runs, timeout, lambada_path, **
             first_new_word = output.replace(detokenize(start_ids[0]), '').split()[0]
             lambada.submit_prediction(i, first_new_word)
 
-    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    # tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
     def detokenize(answer):
         return tokenizer.decode(answer, skip_special_tokens=True)
@@ -41,8 +45,11 @@ def run_pytorch_fp32(model_name, batch_size, num_runs, timeout, lambada_path, **
     model.eval()
     dataset = Lambada(batch_size, tokenize, detokenize, lambada_path)
     # model = apply_jit_trace(model, torch.randint(10000, (5,)))
-    model = apply_jit_trace(model, (dataset.get_input_array()[0],))
-    runner = PyTorchRunnerV2(model)
+    inputs = tokenize("Hello, I'm looking for an employment, ")
+    model.generate = apply_jit_trace_module(model, {"generate": inputs})
+
+    # model = apply_jit_trace(model, (dataset.get_input_array()[0],))
+    runner = PyTorchRunnerV2(model.generate)
     # runner = PyTorchRunner(model, disable_jit_freeze=True, func="generate")
 
     return run_model(run_single_pass, runner, dataset, batch_size, num_runs, timeout)
