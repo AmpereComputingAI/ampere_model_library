0b79157bb3724dbb7f0b29459ac50beb37639d72
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-12-14 12:30:53 +0100

    wip

--- natural_language_processing/text_generation/gpt/run-v2.py
+++ natural_language_processing/text_generation/gpt/run-v2.py
@@ -13,7 +13,7 @@ def run_pytorch_fp32(model_name, batch_size, num_runs, timeout, lambada_path, **
         # output = pytorch_runner.run(None, start_ids, num_beams=2, no_repeat_ngram_size=2,
         #                             early_stopping=True, max_new_tokens=5)
         outputs = pytorch_runner.run(None, start_ids, do_sample=True, max_length=50, top_p=0.95)
-        pytorch_runner.set_task_size(outputs.shape[1] - inputs.input_ids.shape[1])
+        pytorch_runner.set_task_size(outputs.shape[1] - start_ids.shape[1])
         output = detokenize(outputs[0])
 
         for i in range(batch_size):
