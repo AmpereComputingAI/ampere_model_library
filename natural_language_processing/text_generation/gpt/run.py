ac835280836233b9772fe1d00a02de5c77607a14
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2023-12-18 16:07:56 +0100

    wip

--- natural_language_processing/text_generation/gpt/run.py
+++ natural_language_processing/text_generation/gpt/run.py
@@ -12,7 +12,7 @@ def run_pytorch_fp32(model_name, batch_size, num_runs, timeout, lambada_path, **
 
     def run_single_pass(pytorch_runner, lambada):
         start_ids = lambada.get_input_array()[0]
-        output = pytorch_runner.run(inputs=start_ids, max_new_tokens=10)
+        output = pytorch_runner.run(inputs=start_ids, max_new_tokens=10, pad_token_id=tokenizer.pad_token_id)
         pytorch_runner.set_task_size(output.shape[1] - start_ids.shape[1])
         output = detokenize(output[0])
 
@@ -21,6 +21,8 @@ def run_pytorch_fp32(model_name, batch_size, num_runs, timeout, lambada_path, **
             lambada.submit_prediction(i, first_new_word)
 
     tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
 
     def detokenize(answer):
         return tokenizer.decode(answer, skip_special_tokens=True)
