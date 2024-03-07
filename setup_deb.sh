9aab5c8c28223d1bbb07071330fdf6700f79ca97
Jan Grzybek <jan@amperecomputing.com>
2024-03-07 03:00:17 +0100

    test

--- setup_deb.sh
+++ setup_deb.sh
@@ -11,7 +11,7 @@ log() {
 }
 
 ARCH=$( uname -m )
-CPU_FLAGS=$( lscpux | grep "Flags:"  | sed 's/^.*: //' )
+CPU_FLAGS=$( lscpuxx | grep "Flags:"  | sed 's/^.*: //' )
 
 if [ -z ${SCRIPT_DIR+x} ]; then
    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
