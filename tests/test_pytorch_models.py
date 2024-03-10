27d06c1a4d122f9c94cffb989a21f5a4be47c0f5
Jan Grzybek <jan@amperecomputing.com>
2024-03-10 02:29:41 +0100

    wip

--- tests/test_pytorch_models.py
+++ tests/test_pytorch_models.py
@@ -12,7 +12,7 @@ class LLaMA2(unittest.TestCase):
         url = "https://github.com/tloen/alpaca-lora/raw/main/alpaca_data.json"
         self.dataset_path = pathlib.Path(get_downloads_path(), "alpaca_data.json")
         if not self.dataset_path.exists():
-            subprocess.run(f"wget -P {get_downloads_path()} {url}")
+            subprocess.run(f"wget -P {get_downloads_path()} {url}".split())
 
     @unittest.skipIf(psutil.virtual_memory().available / 1024 ** 3 < 100,
                      "too little memory")
@@ -21,7 +21,7 @@ class LLaMA2(unittest.TestCase):
         f1_ref = 0.290
         acc, perf = run_pytorch_fp32(model_name="meta-llama/Llama-2-7b-chat-hf",
                                      batch_size=1, num_runs=50, timeout=None, dataset_path=self.dataset_path)
-        self.assertTrue(acc["f1"]/f1_ref > 0.95)
+        self.assertTrue(acc["f1"] / f1_ref > 0.95)
 
     @unittest.skipIf(psutil.virtual_memory().available / 1024 ** 3 < 100,
                      "too little memory")
@@ -30,7 +30,7 @@ class LLaMA2(unittest.TestCase):
         f1_ref = 0.164
         acc, perf = run_pytorch_fp32(model_name="meta-llama/Llama-2-13b-chat-hf",
                                      batch_size=1, num_runs=50, timeout=None, dataset_path=self.dataset_path)
-        self.assertTrue(acc["f1"]/f1_ref > 0.95)
+        self.assertTrue(acc["f1"] / f1_ref > 0.95)
 
 
 if __name__ == "__main__":
