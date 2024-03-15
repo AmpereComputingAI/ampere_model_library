53685541eab86fe13bc22287d29a3239c2fa0dc5
Daniel Kupnicki <dkupnicki@amperecomputing.com>
2024-03-15 18:29:14 +0000

    add whisper translate

---
+++ speech_recognition/whisper_translate/run.py
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2024, Ampere Computing LLC
+import os
+import sys
+import torch
+
+
+def run_pytorch_fp32(model_name, num_runs, timeout, **kwargs):
+    batch_size = 1
+    sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "whisper"))
+    from utils.benchmark import run_model
+    from utils.misc import print_warning_message
+    from utils.pytorch import PyTorchRunnerV2
+    from utils.speech_recognition.covost2 import Covost2
+    from speech_recognition.whisper.whisper.whisper import load_model
+    from speech_recognition.whisper.whisper.whisper.transcribe import transcribe
+    model = load_model(model_name)
+    model.eval()
+
+    def single_pass_pytorch(_runner, _covost2):
+        array = _covost2.get_input_array()
+        audio = torch.from_numpy(array.astype("float32"))
+        _covost2.submit_translation(
+            _runner.run(batch_size * array.shape[0], audio)["text"].lstrip().replace(".", "")
+        )
+
+    def translate_wrapper(audio):
+        return transcribe(model, audio, verbose=None, task="translate", language="ja")
+
+    runner = PyTorchRunnerV2(translate_wrapper, throughput_only=True)
+    librispeech = Covost2()
+    print_warning_message("Sampling rate Whisper operates at is 16,000 Hz, therefore throughput values below can be "
+                          "divided by 16,000 to derive 'seconds of processed audio per second'")
+    return run_model(single_pass_pytorch, runner, librispeech, batch_size, num_runs, timeout)
+
+
+if __name__ == "__main__":
+    from utils.helpers import DefaultArgParser
+    whisper_variants = ["tiny", "base", "small", "medium", "large"]
+    parser = DefaultArgParser(["pytorch"])
+    parser.require_model_name(whisper_variants)
+
+    run_pytorch_fp32(**vars(parser.parse()))
---
+++ utils/speech_recognition/covost2.py
@@ -0,0 +1,43 @@
+from evaluate import load
+from datasets import load_dataset
+import utils.misc as utils
+from utils.misc import OutOfInstances
+from utils.helpers import Dataset
+
+
+class Covost2(Dataset):
+    sampling_rate = 16000
+
+    def __init__(self, dataset_path=None):
+
+        if dataset_path is None:
+            env_var = "COMMONVOICE_PATH"
+            dataset_path = utils.get_env_variable(
+                env_var, f"Path to CommonVoice directory has not been specified with {env_var} flag")
+
+        self._covost2 = load_dataset("covost2", "ja_en", split="validation", data_dir=dataset_path)
+        self.available_instances = len(self._covost2["audio"])
+        self._idx = 0
+        self._translations = []
+
+    def get_input_array(self):
+        try:
+            return self._covost2["audio"][self._idx]["array"]
+        except IndexError:
+            raise OutOfInstances
+
+    def submit_translation(self, text: str):
+        self._translations.append(text)
+        self._idx += 1
+
+    def reset(self):
+        self._idx = 0
+        self._translations = []
+        return True
+
+    def summarize_accuracy(self):
+        assert len(self._translations) == len(self._covost2["translation"][:self._idx])
+        bleu_score = load("bleu").compute(
+            references=self._covost2["translation"][:self._idx], predictions=self._translations
+        )
+        return {"bleu_score": bleu_score["bleu"]}
