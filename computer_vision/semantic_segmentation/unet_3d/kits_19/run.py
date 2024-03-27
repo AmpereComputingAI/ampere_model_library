bc075b32f6d2a1ef7a09fa3905c5d5642884dff9
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2024-03-27 12:18:21 +0100

    wip

--- computer_vision/semantic_segmentation/unet_3d/kits_19/run.py
+++ computer_vision/semantic_segmentation/unet_3d/kits_19/run.py
@@ -27,38 +27,18 @@ def run_tf_fp(model_path, num_runs, timeout, kits_path):
 
 
 def run_pytorch_fp(model_path, num_runs, timeout, kits_path):
-    from utils.pytorch import PyTorchRunnerV2, apply_jit_script, apply_jit_trace
+    from utils.pytorch import PyTorchRunnerV2
 
     def run_single_pass(pytorch_runner, kits):
         output = pytorch_runner.run(1, torch.from_numpy(np.expand_dims(kits.get_input_array(), axis=0)))
         kits.submit_predictions(tf.convert_to_tensor(output.numpy()))
 
     dataset = KiTS19(dataset_dir_path=kits_path)
-    model = torch.jit.load(model_path, map_location=torch.device('cpu'))
-    model.eval()
+    model = torch.jit.load(model_path, map_location=torch.device('cpu')).eval()
     model = torch.jit.freeze(model)
-    # model = apply_jit_script(model)
-    # model = apply_jit_trace(model, torch.from_numpy(np.expand_dims(dataset.get_input_array(), axis=0)))
     runner = PyTorchRunnerV2(model)
 
     return run_model(run_single_pass, runner, dataset, 1, num_runs, timeout)
-
-
-# def run_pytorch_fp(model_path, num_runs, timeout, kits_path):
-#     from utils.pytorch import PyTorchRunnerV2, apply_jit_script
-#
-#     def run_single_pass(pytorch_runner, kits):
-#         output = pytorch_runner.run(1, torch.from_numpy(np.expand_dims(kits.get_input_array(), axis=0)))
-#         kits.submit_predictions(tf.convert_to_tensor(output.numpy()))
-#
-#     dataset = KiTS19(dataset_dir_path=kits_path)
-#     dlrm.load_state_dict(torch.load(model_path)["state_dict"])
-#     model = torch.load(model_path, map_location=torch.device('cpu'))
-#     model.eval()
-#     model = apply_jit_script(model)
-#     runner = PyTorchRunnerV2(model)
-#
-#     return run_model(run_single_pass, runner, dataset, 1, num_runs, timeout)
 
 
 def run_tf_fp32(model_path, num_runs, timeout, kits_path, **kwargs):
