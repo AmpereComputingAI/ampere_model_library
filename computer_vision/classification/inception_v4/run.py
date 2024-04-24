ea8bae0e23054767e734ade5b957359c2c94c158
Jan Grzybek <jan@amperecomputing.com>
2024-04-24 19:06:30 +0200

    wip

--- computer_vision/classification/inception_v4/run.py
+++ computer_vision/classification/inception_v4/run.py
@@ -1,12 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2024, Ampere Computing LLC
-import argparse
-from utils.benchmark import run_model
-from utils.cv.imagenet import ImageNet
-from utils.misc import print_goodbye_message_and_die, download_ampere_imagenet
 
 
 def parse_args():
+    import argparse
     parser = argparse.ArgumentParser(description="Run Inception v4 model.")
     parser.add_argument("-m", "--model_path",
                         type=str,
@@ -37,6 +34,8 @@ def parse_args():
 
 
 def run_tf_fp(model_path, batch_size, num_runs, timeout, images_path, labels_path):
+    from utils.benchmark import run_model
+    from utils.cv.imagenet import ImageNet
     from utils.tf import TFFrozenModelRunner
 
     def run_single_pass(tf_runner, imagenet):
@@ -66,6 +65,7 @@ def run_tf_fp16(model_path, batch_size, num_runs, timeout, images_path, labels_p
 
 
 def main():
+    from utils.misc import print_goodbye_message_and_die, download_ampere_imagenet
     args = parse_args()
     download_ampere_imagenet()
 
--- tests/test_imports.py
+++ tests/test_imports.py
@@ -10,11 +10,17 @@ class TestImports(unittest.TestCase):
         for path in list(pathlib.Path(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")).rglob("*")):
             if not path.is_file() or any([token not in path.name for token in [".py", "run"]]):
                 continue
+            inside_func = False
             with open(path, "r") as f:
                 for line in f.readlines():
-                    if "import" in line:
+                    if "import" in line and not inside_func:
                         issue_tracker.register(f"Import call(s) found in file \033[91m"
                                                f"{str(path).replace(aml_dir, '')}\033[0m")
                         break
+                    if any([token in line for token in
+                            ["def ", "if __name__ == \"__main__\":", "if __name__ == '__main__'':"]]):
+                        inside_func = True
+                    elif inside_func and " " != line[0]:
+                        inside_func = False
         failure, issues = issue_tracker.is_failure()
         self.assertFalse(failure, issues + f"\nImports should not be done outside of functions in runner files.")
