0da6b17518210038ba1a2849c31c150f112317a6
Karol Kontny <karol@amperecomputing.com>
2024-05-10 07:56:14 +0200

    Changes to run transformers 4.40

--- natural_language_processing/text_generation/alpaca/run.py
+++ natural_language_processing/text_generation/alpaca/run.py
@@ -22,11 +22,13 @@ def run_pytorch(model_path, num_runs, timeout, dataset_path, use_torch_fp16=Fals
     from utils.nlp.alpaca_instruct import AlpacaInstruct
     from utils.pytorch import PyTorchRunnerV2, apply_compile
     from utils.benchmark import run_model
-    from transformers import AutoModelForCausalLM, AutoTokenizer
+    from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 
     def run_single_pass(pytorch_runner, _dataset):
         inputs = encode(_dataset.get_input_string())
-        outputs = pytorch_runner.run(inputs=inputs.input_ids, max_new_tokens=100)
+        config = GenerationConfig()
+        config.max_new_tokens = 100
+        outputs = pytorch_runner.run(inputs=inputs.input_ids, generation_config=config)
         pytorch_runner.set_task_size(outputs.shape[1] - inputs.input_ids.shape[1])
         response = decode(outputs[:, inputs.input_ids.shape[1]:])
         _dataset.submit_prediction(response)
@@ -35,7 +37,7 @@ def run_pytorch(model_path, num_runs, timeout, dataset_path, use_torch_fp16=Fals
     if use_torch_fp16:
         model = model.half()
     model.eval()
-    model.greedy_search = apply_compile(model.greedy_search)
+    model.generate = apply_compile(model.generate)
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     dataset = AlpacaInstruct(1, dataset_path=dataset_path)
