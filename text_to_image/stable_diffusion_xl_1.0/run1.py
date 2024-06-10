4f5dd6e3a1f9a161e112e1dec97be7e94f6d94a3
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2024-06-10 15:42:36 +0200

    wip

--- text_to_image/stable_diffusion_xl_1.0/run1.py
+++ text_to_image/stable_diffusion_xl_1.0/run1.py
@@ -1,12 +1,15 @@
 from diffusers import DiffusionPipeline
 import torch
 
+torch.set_num_threads(128)
+
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
-pipe.to("cuda")
+pipe.to("cpu")
 
 # if using torch < 2.0
 # pipe.enable_xformers_memory_efficient_attention()
 
 prompt = "An astronaut riding a green horse"
 
-images = pipe(prompt=prompt).images[0]
+with torch.no_grad():
+    images = pipe(prompt=prompt).images[0]
