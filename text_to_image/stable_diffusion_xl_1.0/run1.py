516be169953a84ba0861474a9e4f056244f262d5
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2024-06-10 19:45:30 +0200

    wip

--- text_to_image/stable_diffusion_xl_1.0/run1.py
+++ text_to_image/stable_diffusion_xl_1.0/run1.py
@@ -5,20 +5,21 @@ from utils.pytorch import apply_jit_trace
 
 torch.set_num_threads(128)
 
-pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",
                                          torch_dtype=torch.float16,
                                          use_safetensors=True,
                                          variant="fp16")
 
 pipe.to("cpu")
-pipe.eval()
+unet = pipe.unet
+unet.eval()
 
 example_input = torch.randn(1, 3, 512, 512)
 # traced_model = torch.jit.trace(pipe, example_input)
 
 # frozen_model = torch.jit.freeze(traced_model)
 
-model = apply_jit_trace(pipe, example_input)
+model = apply_jit_trace(unet, example_input)
 # if using torch < 2.0
 # pipe.enable_xformers_memory_efficient_attention()
 
