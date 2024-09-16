eb80d5435f4cd79ef8a1c3b9fba9dc46446dd100
Jan Grzybek <jan@amperecomputing.com>
2024-09-16 14:46:13 +0200

    Update setup_deb.sh

--- setup_deb.sh
+++ setup_deb.sh
@@ -77,11 +77,11 @@ apt install -y autoconf autogen automake build-essential libasound2-dev \
 apt remove -y libsndfile1
 git clone -b 1.2.2 https://github.com/libsndfile/libsndfile.git && cd libsndfile/ && autoreconf -vif && ./configure --enable-werror && make -j && make install && ldconfig && cd .. && rm -rf libsndfile
 
-if [ "$(PYTHONPATH=$SCRIPT_DIR python3 -c 'from cpuinfo import get_cpu_info; from benchmark import which_ampere_cpu; cpu = which_ampere_cpu(get_cpu_info()["flags"], 1); print("AmpereOne" in cpu)')" == "True" ]; then
-    # Only on AmpereOne family
-    pip3 install --break-system-packages --upgrade -r "$(dirname "$0")/requirements-ampereone.txt" ||
-        pip3 install --upgrade -r "$(dirname "$0")/requirements-ampereone.txt"
-fi
+# if [ "$(PYTHONPATH=$SCRIPT_DIR python3 -c 'from cpuinfo import get_cpu_info; from benchmark import which_ampere_cpu; cpu = which_ampere_cpu(get_cpu_info()["flags"], 1); print("AmpereOne" in cpu)')" == "True" ]; then
+#     # Only on AmpereOne family
+#     pip3 install --break-system-packages --upgrade -r "$(dirname "$0")/requirements-ampereone.txt" ||
+#         pip3 install --upgrade -r "$(dirname "$0")/requirements-ampereone.txt"
+# fi
 
 ARCH=$ARCH python3 "$SCRIPT_DIR"/utils/setup/install_frameworks.py
 
