48fe49745a30b08adbba43e3bdeb4fe08e0c2766
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2025-11-04 13:19:07 +0100

    wip

--- tests/test_pytorch_models.py
+++ tests/test_pytorch_models.py
@@ -270,19 +270,19 @@ class Inception(unittest.TestCase):
     def setUp(self):
         self.dataset_path, self.labels_path = download_imagenet_maybe()
 
-    # def test_inception_v3(self):
-    #     from computer_vision.classification.inception_v3.run import run_pytorch_fp32
-    #
-    #     def wrapper(**kwargs):
-    #         kwargs["q"].put(run_pytorch_fp32(**kwargs)[0])
-    #
-    #     top_1_ref, top_5_ref = 0.765, 0.932
-    #     acc = run_process(wrapper, {"model_name": "inception_v3", "images_path": self.dataset_path,
-    #                                 "labels_path": self.labels_path, "batch_size": 32, "num_runs": 10,
-    #                                 "timeout": None,
-    #                                 "disable_jit_freeze": False})
-    #     self.assertTrue(acc["top_1_acc"] / top_1_ref > 0.95)
-    #     self.assertTrue(acc["top_5_acc"] / top_5_ref > 0.95)
+    def test_inception_v3(self):
+        from computer_vision.classification.inception_v3.run import run_pytorch_fp32
+
+        def wrapper(**kwargs):
+            kwargs["q"].put(run_pytorch_fp32(**kwargs)[0])
+
+        top_1_ref, top_5_ref = 0.765, 0.932
+        acc = run_process(wrapper, {"model_name": "inception_v3", "images_path": self.dataset_path,
+                                    "labels_path": self.labels_path, "batch_size": 32, "num_runs": 10,
+                                    "timeout": None,
+                                    "disable_jit_freeze": False})
+        self.assertTrue(acc["top_1_acc"] / top_1_ref > 0.95)
+        self.assertTrue(acc["top_5_acc"] / top_5_ref > 0.95)
 
 
 class ResNet(unittest.TestCase):
