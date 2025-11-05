0a9b49b869cdbb47cd24e03852928da519bd4f48
Marcel Wilnicki <marcel.wilnicki@gmail.com>
2025-11-05 13:10:39 +0100

    wip

--- tests/test_pytorch_models.py
+++ tests/test_pytorch_models.py
@@ -48,23 +48,23 @@ class LLaMA2(unittest.TestCase):
 
         self.wrapper = wrapper
 
-    # @unittest.skipIf(psutil.virtual_memory().available / 1024 ** 3 < 100, "too little memory")
-    # @unittest.skipUnless('_aio_profiler_print' in dir(torch._C), "Ampere optimized PyTorch required")
-    # def test_llama2_7b(self):
-    #     f1_ref = 0.330
-    #     acc = run_process(self.wrapper,
-    #                       {"model_name": "meta-llama/Llama-2-7b-chat-hf", "batch_size": 1, "num_runs": 50,
-    #                        "timeout": None, "dataset_path": self.dataset_path})
-    #     self.assertTrue(acc["f1"] / f1_ref > 0.95)
-    #
-    # @unittest.skipIf(psutil.virtual_memory().available / 1024 ** 3 < 200, "too little memory")
-    # @unittest.skipUnless('_aio_profiler_print' in dir(torch._C), "Ampere optimized PyTorch required")
-    # def test_llama2_13b(self):
-    #     f1_ref = 0.261
-    #     acc = run_process(self.wrapper,
-    #                       {"model_name": "meta-llama/Llama-2-13b-chat-hf", "batch_size": 1, "num_runs": 50,
-    #                        "timeout": None, "dataset_path": self.dataset_path})
-    #     self.assertTrue(acc["f1"] / f1_ref > 0.95)
+    @unittest.skipIf(psutil.virtual_memory().available / 1024 ** 3 < 100, "too little memory")
+    @unittest.skipUnless('_aio_profiler_print' in dir(torch._C), "Ampere optimized PyTorch required")
+    def test_llama2_7b(self):
+        f1_ref = 0.330
+        acc = run_process(self.wrapper,
+                          {"model_name": "meta-llama/Llama-2-7b-chat-hf", "batch_size": 1, "num_runs": 50,
+                           "timeout": None, "dataset_path": self.dataset_path})
+        self.assertTrue(acc["f1"] / f1_ref > 0.95)
+
+    @unittest.skipIf(psutil.virtual_memory().available / 1024 ** 3 < 200, "too little memory")
+    @unittest.skipUnless('_aio_profiler_print' in dir(torch._C), "Ampere optimized PyTorch required")
+    def test_llama2_13b(self):
+        f1_ref = 0.261
+        acc = run_process(self.wrapper,
+                          {"model_name": "meta-llama/Llama-2-13b-chat-hf", "batch_size": 1, "num_runs": 50,
+                           "timeout": None, "dataset_path": self.dataset_path})
+        self.assertTrue(acc["f1"] / f1_ref > 0.95)
 
 
 class Alpaca(unittest.TestCase):
